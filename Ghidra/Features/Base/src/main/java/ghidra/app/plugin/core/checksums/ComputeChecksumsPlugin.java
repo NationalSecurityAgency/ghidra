61b1f7b5ed6f0ee18ed61af2201e11fbc9027123
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2019-05-08 14:02:41 -0400

    GT-2845 - Checksums - fix for potential concurrent mod exception

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/checksums/ComputeChecksumsPlugin.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/checksums/ComputeChecksumsPlugin.java
@@ -107,33 +107,22 @@ public class ComputeChecksumsPlugin extends ProgramPlugin {
 	 */
 	@Override
 	protected void selectionChanged(ProgramSelection selection) {
-		super.selectionChanged(selection);
-		if (provider != null) {
-			provider.setSelection(hasSelection());
-		}
+		provider.setSelection(hasSelection());
 	}
 
 	/**
-	 * Get's the provider associated with this plugin.
-	 * @return the provider associated with this plugin.
+	 * Returns the current program selection
+	 * @return the current program selection
 	 */
-	public ComputeChecksumsProvider getProvider() {
-		return provider;
-	}
-
-	/**
-	 * Returns the current program selection.
-	 * @return the current program selection.
-	 */
-	public ProgramSelection getSelection() {
+	ProgramSelection getSelection() {
 		return currentSelection;
 	}
 
 	/**
-	 * Returns true if the current program has a selection.
-	 * @return true if the current program has a selection.
+	 * Returns true if the current program has a selection
+	 * @return true if the current program has a selection
 	 */
-	public boolean hasSelection() {
+	boolean hasSelection() {
 		ProgramSelection selection = getSelection();
 		return selection != null && !selection.isEmpty();
 	}
