a1563c31042b3a2d87b019bcfe310899a3edfa4f
Markus Pieton <marpie@a12d404.net>
2020-02-23 13:21:45 +0100

    added license header and check if already disassembled

--- Ghidra/Features/MicrosoftCodeAnalyzer/src/main/java/ghidra/app/plugin/prototype/MicrosoftCodeAnalyzerPlugin/CfgAnalyzer.java
+++ Ghidra/Features/MicrosoftCodeAnalyzer/src/main/java/ghidra/app/plugin/prototype/MicrosoftCodeAnalyzerPlugin/CfgAnalyzer.java
@@ -1,3 +1,18 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ghidra.app.plugin.prototype.MicrosoftCodeAnalyzerPlugin;
 
 import java.util.ArrayList;
@@ -69,15 +84,14 @@ public class CfgAnalyzer extends AbstractAnalyzer {
 		FunctionManager funcMgr = program.getFunctionManager();
 		Command cmd = null;
 		for (Address target : getFunctionAddressesFromTable(program, tableData)) {
-			if (funcMgr.getFunctionAt(target) != null) {
-				// if there already is a function, just bail...
-				continue;
+			if (listing.getInstructionAt(target) == null) {
+				cmd = new DisassembleCommand(target, null, true);
+				cmd.applyTo(program);
 			}
-			cmd = new DisassembleCommand(target, null, true);
-			cmd.applyTo(program);
-
-			cmd = new CreateFunctionCmd(target);
-			cmd.applyTo(program);
+			if (funcMgr.getFunctionAt(target) == null) {
+				cmd = new CreateFunctionCmd(target);
+				cmd.applyTo(program);
+			}
 		}
 
 		return true;
