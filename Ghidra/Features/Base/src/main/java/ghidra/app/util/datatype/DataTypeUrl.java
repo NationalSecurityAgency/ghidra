f497b9514dddae6853f2377d48ecedc74d9f7d75
dev747368 <48332326+dev747368@users.noreply.github.com>
2020-03-02 13:26:25 -0500

    GT-3574 fix DataTypeUrl to handle data types with '.'s in name
    (ie. "something.conflict")
    
    Pretty much allow anything for the name (other than no name value).

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/datatype/DataTypeUrl.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/datatype/DataTypeUrl.java
@@ -36,7 +36,7 @@ public class DataTypeUrl {
 	// see javadoc for format
 	private static String PROTOCOL = "datatype";
 	private static Pattern URL_PATTERN =
-		Pattern.compile(PROTOCOL + ":/(\\d+)\\?uid=(\\d*)&name=(\\w+)");
+		Pattern.compile(PROTOCOL + ":/(\\d+)\\?uid=(\\d*)&name=(.+)");
 
 	private UniversalID dataTypeManagerId;
 	private UniversalID dataTypeId;
