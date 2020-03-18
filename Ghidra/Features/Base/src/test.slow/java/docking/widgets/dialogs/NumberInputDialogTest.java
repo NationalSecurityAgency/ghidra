378c9a5ae045c38fb2eae37855e9ec7b1a94f541
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2020-03-18 13:25:12 -0400

    GT-3607 - Added a version of the Number Input dialog that allows for numbers larger than int

--- Ghidra/Features/Base/src/test.slow/java/docking/widgets/dialogs/NumberInputDialogTest.java
+++ Ghidra/Features/Base/src/test.slow/java/docking/widgets/dialogs/NumberInputDialogTest.java
@@ -15,251 +15,42 @@
  */
 package docking.widgets.dialogs;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-import java.awt.Image;
-import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JTextField;
-
-import org.junit.After;
 import org.junit.Test;
 
-import docking.DockingWindowManager;
-import docking.test.AbstractDockingTest;
-import docking.widgets.textfield.IntegerTextField;
-import ghidra.test.DummyTool;
-
-public class NumberInputDialogTest extends AbstractDockingTest {
-
-	private DockingWindowManager dwm =
-		new DockingWindowManager(new DummyTool(), (List<Image>) null);
-	private NumberInputDialog dialog;
-	private JButton okButton;
-	private JTextField textField;
-
-	@After
-	public void tearDown() throws Exception {
-		if (dialog != null) {
-			runSwing(() -> dialog.close());
-		}
-	}
+/**
+ * {@link NumberInputDialog} test. 
+ * 
+ * <p>Note: most of the tests for the base class are in {@link BigIntegerNumberInputDialogTest}.
+ */
+public class NumberInputDialogTest extends AbstractNumberInputDialogTest {
 
 	@Test
-	public void testOkWithInitialValue() throws Exception {
-
+	public void testTypeIntTooBigWithOverflow() {
 		int initial = 2;
-		int min = 2;
-		int max = 5;
-		createAndShowDialog(initial, min, max);
+		createAndShowDialog(initial, 0, Integer.MAX_VALUE);
 
-		clickOK();
+		String okInt = "500000000";
+		setText(okInt);
+		assertTrue(okButton.isEnabled());
 
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
+		setText(okInt + "0");
+		assertEquals("Value must be between 0 and " + Integer.MAX_VALUE, dialog.getStatusText());
 
-		assertEquals("The returned value is not the expected value", 2, dialog.getValue());
+		setText(okInt + "00");
+		assertEquals("Value must be between 0 and " + Integer.MAX_VALUE, dialog.getStatusText());
+
+		setText(okInt + "000");
+		assertEquals("Value must be between 0 and " + Integer.MAX_VALUE, dialog.getStatusText());
+
+		int valid = Integer.MAX_VALUE - 1;
+		setText(Integer.toString(valid));
+		oK();
+		assertEquals(valid, getValue());
 	}
 
-	@Test
-	public void testOkWithNewAllowedValue() throws Exception {
-		int initial = 2;
-		int min = 2;
-		int max = 5;
-		createAndShowDialog(initial, min, max);
-
-		setText("4");
-
-		clickOK();
-
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", 4, dialog.getValue());
-	}
-
-	@Test
-	public void testTypingInHigherThanAllowed() {
-		int initial = 2;
-		int min = 2;
-		int max = 5;
-
-		createAndShowDialog(initial, min, max);
-
-		setText("7");
-
-		assertTrue(!okButton.isEnabled());
-
-		assertEquals("Value must be between 2 and 5", dialog.getStatusText());
-	}
-
-	@Test
-	public void testTypingInLowerThanAllowed() {
-		int initial = 2;
-		int min = 2;
-		int max = 5;
-
-		createAndShowDialog(initial, min, max);
-
-		setText("1");
-
-		assertTrue(!okButton.isEnabled());
-
-		assertEquals("Value must be between 2 and 5", dialog.getStatusText());
-
-	}
-
-	@Test
-	public void testTypingValidHex() {
-		int initial = 2;
-		int min = 2;
-		int max = 5;
-
-		createAndShowDialog(initial, min, max);
-
-		setText("0x4");
-		clickOK();
-
-		assertTrue("The dialog is open after pressing 'OK' with a valid hex value",
-			!dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", 4, dialog.getValue());
-	}
-
-	@Test
-	public void testTypeHexTooBig() {
-		int initial = 2;
-		int min = 2;
-		int max = 5;
-
-		createAndShowDialog(initial, min, max);
-
-		setText("0x7");
-
-		assertTrue(!okButton.isEnabled());
-
-		assertEquals("Value must be between 2 and 5", dialog.getStatusText());
-
-	}
-
-	@Test
-	public void testTypeLargeHexValue() {
-		int initial = 2;
-		int min = 2;
-		int max = Integer.MAX_VALUE;
-		createAndShowDialog(initial, min, max);
-
-		setText("0xfff");
-
-		clickOK();
-
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", 4095, dialog.getValue());
-	}
-
-	@Test
-	public void testTypingNegativeValidNumber() {
-		int initial = 2;
-		int min = -5;
-		int max = 10;
-		createAndShowDialog(initial, min, max);
-
-		setText("-3");
-
-		clickOK();
-
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", -3, dialog.getValue());
-	}
-
-	@Test
-	public void testTypingNegativeValidHexNumber() {
-		int initial = 2;
-		int min = -5;
-		int max = 10;
-		createAndShowDialog(initial, min, max);
-
-		setText("-0x3");
-
-		clickOK();
-
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", -3, dialog.getValue());
-	}
-
-	@Test
-	public void testSettingNoMaximum() {
-
-		int initial = 1;
-		int min = 1;
-		createAndShowDialog(initial, min);
-
-		int max = dialog.getMax();
-		assertThat(max, is(Integer.MAX_VALUE));
-
-		setText(Integer.toString(min + 1));
-
-		clickOK();
-
-		assertFalse("The dialog is open after pressing 'OK' with valid value", dialog.isVisible());
-
-		assertEquals("The returned value is not the entered value", (min + 1), dialog.getValue());
-	}
-
-	@Test
-	public void testSettingInvalidMaximum() {
-
-		int initial = 1;
-		int min = 2;
-		int max = min - 1;
-
-		try {
-			new NumberInputDialog(null, initial, min, max);
-			fail("Expected an exception with a max lower than min");
-		}
-		catch (IllegalArgumentException e) {
-			// good
-		}
-	}
-
-	private void createAndShowDialog(int initialValue, int min, int max) {
-		dialog = new NumberInputDialog(null, initialValue, min, max);
-		showDialogOnSwingWithoutBlocking(dialog);
-		okButton = (JButton) getInstanceField("okButton", dialog);
-		textField = getTextFieldForDialog(dialog);
-	}
-
-	private void createAndShowDialog(int initial, int min) {
-		dialog = new NumberInputDialog(null, initial, min);
-		showDialogOnSwingWithoutBlocking(dialog);
-		okButton = (JButton) getInstanceField("okButton", dialog);
-		textField = getTextFieldForDialog(dialog);
-	}
-
-	private void clickOK() {
-		runSwing(() -> okButton.doClick());
-	}
-
-	private void setText(String value) {
-		triggerText(textField, value);
-	}
-
-	private void showDialogOnSwingWithoutBlocking(NumberInputDialog theDialog) {
-
-		runSwing(() -> {
-
-			dwm.showDialog(theDialog);
-			theDialog.getValue();
-		}, false);
-
-		waitForDialogComponent(null, NumberInputDialog.class, DEFAULT_WINDOW_TIMEOUT);
-	}
-
-	private JTextField getTextFieldForDialog(NumberInputDialog theDialog) {
-		IntegerTextField inputField = theDialog.getNumberInputField();
-		return (JTextField) getInstanceField("textField", inputField);
+	private int getValue() {
+		return ((NumberInputDialog) dialog).getValue();
 	}
 }
--- Ghidra/Framework/Docking/src/main/java/docking/widgets/dialogs/NumberInputDialog.java
+++ Ghidra/Framework/Docking/src/main/java/docking/widgets/dialogs/NumberInputDialog.java
@@ -15,22 +15,12 @@
  */
 package docking.widgets.dialogs;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.math.BigInteger;
-
-import javax.swing.*;
-
-import docking.DialogComponentProvider;
-import docking.DockingWindowManager;
-import docking.widgets.label.GDLabel;
-import docking.widgets.textfield.IntegerTextField;
-
 /**
  * <P>DialogComponentProvider that provides information to create a modal dialog
  * to prompt for a number (int) to be input by the user.</P>
  *
- * <P>If an initial value is specified it is not in the range of min,max, it will be set to the min.</P>
+ * <P>If an initial value is specified it is not in the range of min,max, it will be set to the 
+ * min.</P>
  *
  * <P>If the maximum value indicated is less than the minimum then the max
  * is the largest positive integer. Otherwise the maximum valid value is
@@ -55,280 +45,56 @@ import docking.widgets.textfield.IntegerTextField;
  * </code>
  * </pre>
  */
-public class NumberInputDialog extends DialogComponentProvider {
+public class NumberInputDialog extends AbstractNumberInputDialog {
 
-	private boolean wasCancelled = false;
-	private IntegerTextField numberInputField;
-	private int min;
-	private int max;
-	private JLabel label;
-	private String defaultMessage;
-
+	/**
+	 * Constructs a new NumberInputDialog
+	 *
+	 * @param entryType item type the number indicates
+	 *                  (i.e. "duplicates", "items", or "elements")
+	 * @param initial default value displayed in the text field
+	 * @param min minimum value allowed
+	 */
 	public NumberInputDialog(String entryType, int initial, int min) {
 		this("Enter Number", buildDefaultPrompt(entryType, min, min - 1), initial, min,
 			Integer.MAX_VALUE, false);
 	}
 
 	/**
-	 * Constructs a new NumberInputDialog.
+	 * Constructs a new NumberInputDialog
 	 *
 	 * @param entryType item type the number indicates
-	 *                  (i.e. "duplicates", "items", or "elements").
-	 * @param initial default value displayed in the text field.
-	 * @param min minimum value allowed.
-	 * @param max maximum value allowed.
+	 *                  (i.e. "duplicates", "items", or "elements")
+	 * @param initial default value displayed in the text field
+	 * @param min minimum value allowed
+	 * @param max maximum value allowed
 	 */
 	public NumberInputDialog(String entryType, int initial, int min, int max) {
 		this("Enter Number", buildDefaultPrompt(entryType, min, max), initial, min, max, false);
 	}
 
 	/**
-	 * Create a numberInputDialog where the the min is 0 and the max is INTEGER.MAX_VALUE
-	 * @param title the title of the dialog
-	 * @param prompt the prompt in the dialog
-	 * @param initialValue the initial value.  If null, the text input will be blank.
-	 */
-	public NumberInputDialog(String title, String prompt, Integer initialValue) {
-		this(title, prompt, initialValue, 0, Integer.MAX_VALUE, false);
-	}
-
-	/**
-	 * Show a number input dialog.
-	 * @param title The title of the dialog.
-	 * @param prompt the prompt to display before the number input field.
-	 * @param initialValue the default value to display, null will leave the field blank.
-	 * @param min the minimum allowed value of the field.
-	 * @param max the maximum allowed value of the field.
-	 * @param showAsHex if true, the initial value will be displayed as hex.
+	 * Show a number input dialog
+	 * @param title The title of the dialog
+	 * @param prompt the prompt to display before the number input field
+	 * @param initialValue the default value to display, null will leave the field blank
+	 * @param min the minimum allowed value of the field
+	 * @param max the maximum allowed value of the field
+	 * @param showAsHex if true, the initial value will be displayed as hex
 	 */
 	public NumberInputDialog(String title, String prompt, Integer initialValue, int min, int max,
 			boolean showAsHex) {
-		super(title, true, true, true, false);
-
-		this.min = min;
-		if (max < min) {
-			throw new IllegalArgumentException(
-				"'min' cannot be less than 'max'. 'min' = " + min + ", 'max' = " + max);
-		}
-		this.max = max;
-
-		addWorkPanel(buildMainPanel(prompt, showAsHex));
-		addOKButton();
-		addCancelButton();
-		setRememberLocation(false);
-		setRememberSize(false);
-
-		initializeDefaultValue(initialValue);
-
-		selectAndFocusText();
-	}
-
-	private static String nonNull(String s) {
-		if (s == null) {
-			return "items";
-		}
-		return s;
+		super(title, prompt, initialValue, min, max, showAsHex);
 	}
 
 	/**
-	 * Define the Main panel for the dialog here.
-	 * @param showAsHex
-	 * @return JPanel the completed <CODE>Main Panel</CODE>
-	 */
-	protected JPanel buildMainPanel(String prompt, boolean showAsHex) {
-		JPanel panel = createPanel(prompt);
-		numberInputField.addActionListener(e -> okCallback());
-
-		if (showAsHex) {
-			numberInputField.setHexMode();
-		}
-		if (min >= 0) {
-			numberInputField.setAllowNegativeValues(false);
-		}
-		return panel;
-	}
-
-	/**
-	 * Gets called when the user clicks on the OK Action for the dialog.
-	 */
-	@Override
-	protected void okCallback() {
-		if (checkInput()) {
-			close();
-		}
-	}
-
-	/**
-	 * Gets called when the user clicks on the Cancel Action for the dialog.
-	 */
-	@Override
-	protected void cancelCallback() {
-		wasCancelled = true;
-		close();
-	}
-
-	/**
-	 * Return whether the user cancelled the input dialog.
-	 */
-	public boolean wasCancelled() {
-		return wasCancelled;
-	}
-
-	private void initializeDefaultValue(Integer initial) {
-		if (initial == null) {
-			return;
-		}
-		int value = initial.intValue();
-		// Adjust the initial value if it is not valid.
-		if (value < min) {
-			value = min;
-		}
-		else if (value > max) {
-			value = max;
-		}
-		numberInputField.setValue(value);
-	}
-
-	private void selectAndFocusText() {
-		SwingUtilities.invokeLater(() -> {
-
-			numberInputField.requestFocus();
-			numberInputField.selectAll();
-		});
-
-	}
-
-	/**
-	 * <code>show</code> displays the dialog, gets the user input
-	 *
-	 * @return false if the user cancelled the operation
-	 */
-	public boolean show() {
-		Component parent = DockingWindowManager.getActiveInstance().getActiveComponent();
-		DockingWindowManager.showDialog(parent, this);
-		return !wasCancelled;
-	}
-
-	/**
-	 * Convert the input to an int value.
-	 * @throws NumberFormatException if entered value cannot be parsed.
+	 * Convert the input to an int value
+	 * @return the int value
+	 * @throws NumberFormatException if entered value cannot be parsed
+	 * @throws IllegalStateException if the dialog was cancelled
 	 */
 	public int getValue() {
-		if (wasCancelled()) {
-			throw new IllegalStateException();
-		}
-		return numberInputField.getIntValue();
+		return getIntValue();
 	}
 
-	/**
-	 * Sets the value in the input field to the indicated value.
-	 */
-	public void setInput(int value) {
-		numberInputField.setValue(value);
-	}
-
-	/**
-	 * Sets the default message to be displayed when valid values are in the text fields.
-	 * @param defaultMessage the message to be displayed when valid values are in the text fields.
-	 */
-	public void setDefaultMessage(String defaultMessage) {
-		this.defaultMessage = defaultMessage;
-		setStatusText(defaultMessage);
-	}
-
-	/**
-	 * Return the minimum acceptable value.
-	 */
-	public int getMin() {
-		return min;
-	}
-
-	/**
-	 * Return the maximum acceptable value.
-	 */
-	public int getMax() {
-		return max;
-	}
-
-//==================================================================================================
-// Test Methods
-//==================================================================================================
-
-	IntegerTextField getNumberInputField() {
-		return numberInputField;
-	}
-
-//==================================================================================================
-// Private Methods
-//==================================================================================================	
-
-	/**
-	 * Create the main panel.
-	 */
-	private JPanel createPanel(String prompt) {
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-		label = new GDLabel(prompt);
-		numberInputField = new IntegerTextField(12);
-		numberInputField.addChangeListener(e -> updateOKButtonEnablement());
-
-		// Actually assemble the parts into a status panel.
-		panel.add(label, BorderLayout.WEST);
-		panel.add(numberInputField.getComponent(), BorderLayout.CENTER);
-
-		return panel;
-	}
-
-	protected void updateOKButtonEnablement() {
-		clearStatusText();
-		BigInteger value = numberInputField.getValue();
-		if (value == null) {
-			setOkEnabled(false);
-			if (defaultMessage != null) {
-				setStatusText(defaultMessage);
-			}
-			else {
-				setStatusText("Enter a value between " + min + " and " + max);
-			}
-			return;
-		}
-		setOkEnabled(checkInput());
-	}
-
-	/**
-	 * Check the entry;
-	 *
-	 * @return boolean true if input is OK
-	 */
-	private boolean checkInput() {
-		int value = numberInputField.getIntValue();
-		return checkDecimalRange(value);
-	}
-
-	private boolean checkDecimalRange(int decimalValue) {
-
-		if (decimalValue >= min && decimalValue <= max) {
-			if (defaultMessage != null) {
-				setStatusText(defaultMessage);
-			}
-			return true;
-		}
-		setStatusText("Value must be between " + min + " and " + max);
-		return false;
-	}
-
-	private static String buildDefaultPrompt(String entryType, int min, int max) {
-		String type = nonNull(entryType);
-		if (min == 0 && max == Integer.MAX_VALUE) {
-			// full range
-			return "Enter number of " + type + ": ";
-		}
-		else if (max == Integer.MAX_VALUE) {
-			return "Enter number of " + type + " (minimum is " + min + ") : ";
-		}
-		else {
-			return "Enter number of " + type + " (" + min + ", " + max + ") : ";
-		}
-	}
 }
