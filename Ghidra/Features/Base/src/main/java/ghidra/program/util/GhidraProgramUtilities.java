eb5b7bada8a5d57cc679f2010c81c66869d4d892
astrelsky <ajs222@njit.edu>
2020-04-11 20:25:27 -0400

    Corrected confusing shouldAskToAnalyze description

--- Ghidra/Features/Base/src/main/java/ghidra/program/util/GhidraProgramUtilities.java
+++ Ghidra/Features/Base/src/main/java/ghidra/program/util/GhidraProgramUtilities.java
@@ -39,9 +39,9 @@ public class GhidraProgramUtilities {
 	}
 
 	/**
-	 * Returns true if the program contains the analyzed flag.
+	 * Returns true if the program does not contain the analyzed flag.
 	 * @param program the program to check for the property
-	 * @return true if the program contains the analyzed flag
+	 * @return true if the program does not contain the analyzed flag
 	 */
 	public static boolean shouldAskToAnalyze(Program program) {
 		try {
