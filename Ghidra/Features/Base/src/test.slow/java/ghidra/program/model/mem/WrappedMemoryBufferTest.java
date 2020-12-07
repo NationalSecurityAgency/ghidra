ebe4587dbd116b25f97d95c4d393ccc9d8435215
emteere <47253321+emteere@users.noreply.github.com>
2020-12-07 17:05:13 -0500

    GP-486_emteere fixed WrappedMemBuffer wrapping a buffer with a non-zero offset and calling getInt()

--- Ghidra/Features/Base/src/test.slow/java/ghidra/program/model/mem/WrappedMemoryBufferTest.java
+++ Ghidra/Features/Base/src/test.slow/java/ghidra/program/model/mem/WrappedMemoryBufferTest.java
@@ -80,79 +80,226 @@ public class WrappedMemoryBufferTest extends AbstractGhidraHeadedIntegrationTest
 		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 0, 0, 0, 0 },
 			bytes);
 	}
-	
+
 	@Test
 	public void testGetBytesBuffered() throws Exception {
 		loadProgram("notepad");
 
 		Address minAddr = program.getMinAddress();
 		Address maxAddr = program.getMaxAddress();
-		setBytes(minAddr, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
+		setBytes(minAddr,
+			new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
 
 		memBuf = new MemoryBufferImpl(program.getMemory(), minAddr);
 		memBuf = new WrappedMemBuffer(memBuf, 4, 0);
 		byte[] bytes = new byte[6];
 		// test get too many for cache
 		assertEquals(6, memBuf.getBytes(bytes, 2));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 2, 3, 4, 5, 6, 7 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 2, 3, 4, 5, 6, 7 }, bytes);
 
 		// test not in buffer
 		memBuf = new WrappedMemBuffer(memBuf, 6, 0);
 		assertEquals(6, memBuf.getBytes(bytes, 0));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 0, 1, 2, 3, 4, 5 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 0, 1, 2, 3, 4, 5 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 2));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 2, 3, 4, 5, 6, 7 },
-			bytes);		
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 2, 3, 4, 5, 6, 7 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 8));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 8, 9, 10, 11, 12, 13 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 8, 9, 10, 11, 12, 13 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 1));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 1, 2, 3, 4, 5, 6 },
-			bytes);
-		
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 1, 2, 3, 4, 5, 6 }, bytes);
+
 		assertEquals(10, memBuf.getByte(10));
 		assertEquals(0, memBuf.getByte(0));
 		assertEquals(5, memBuf.getByte(5));
 		assertEquals(6, memBuf.getByte(6));
 	}
-	
+
 	@Test
 	public void testGetBytesNoBuffered() throws Exception {
 		loadProgram("notepad");
 
 		Address minAddr = program.getMinAddress();
 		Address maxAddr = program.getMaxAddress();
-		setBytes(minAddr, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
+		setBytes(minAddr,
+			new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
 
 		memBuf = new MemoryBufferImpl(program.getMemory(), minAddr);
 		memBuf = new WrappedMemBuffer(memBuf, 0, 0);
 		byte[] bytes = new byte[6];
 		// test get too many for cache
 		assertEquals(6, memBuf.getBytes(bytes, 2));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 2, 3, 4, 5, 6, 7 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 2, 3, 4, 5, 6, 7 }, bytes);
 
 		// test not in buffer
 		memBuf = new WrappedMemBuffer(memBuf, 0, 0);
 		assertEquals(6, memBuf.getBytes(bytes, 0));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 0, 1, 2, 3, 4, 5 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 0, 1, 2, 3, 4, 5 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 2));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 2, 3, 4, 5, 6, 7 },
-			bytes);		
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 2, 3, 4, 5, 6, 7 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 8));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 8, 9, 10, 11, 12, 13 },
-			bytes);
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 8, 9, 10, 11, 12, 13 }, bytes);
 		assertEquals(6, memBuf.getBytes(bytes, 1));
-		Assert.assertArrayEquals("Unexpected bytes read from memBuf", new byte[] { 1, 2, 3, 4, 5, 6 },
-			bytes);
-		
+		Assert.assertArrayEquals("Unexpected bytes read from memBuf",
+			new byte[] { 1, 2, 3, 4, 5, 6 }, bytes);
+
 		assertEquals(10, memBuf.getByte(10));
 		assertEquals(0, memBuf.getByte(0));
 		assertEquals(5, memBuf.getByte(5));
 		assertEquals(6, memBuf.getByte(6));
+	}
+
+	@Test
+	public void testGetIntBuffered() throws Exception {
+		loadProgram("notepad");
+		int value;
+
+		Address minAddr = program.getMinAddress();
+		Address maxAddr = program.getMaxAddress();
+		setBytes(minAddr, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+			18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 });
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 3, 0);
+
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x00010203);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x01020304);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x03040506);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x08090A0B);
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 4, 0);
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x00010203);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x01020304);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x03040506);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x08090A0B);
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 5, 0);
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x00010203);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x01020304);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x03040506);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x08090A0B);
+
+		memBuf = new MemoryBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 3, 4);
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x04050607);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x05060708);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x0708090A);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x0C0D0E0F);
+
+		memBuf = new MemoryBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 4, 4);
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x04050607);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x05060708);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x0708090A);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x0C0D0E0F);
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 5, 4);
+		value = memBuf.getInt(0);
+		assertEquals(value, 0x04050607);
+		value = memBuf.getInt(1);
+		assertEquals(value, 0x05060708);
+		value = memBuf.getInt(3);
+		assertEquals(value, 0x0708090A);
+		value = memBuf.getInt(8);
+		assertEquals(value, 0x0C0D0E0F);
+	}
+
+	@Test
+	public void testGetShortBuffered() throws Exception {
+		loadProgram("notepad");
+		int value;
+
+		Address minAddr = program.getMinAddress();
+		Address maxAddr = program.getMaxAddress();
+		setBytes(minAddr, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+			18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 });
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 3, 0);
+
+		value = memBuf.getShort(0);
+		assertEquals(value, 0x0001);
+		value = memBuf.getShort(1);
+		assertEquals(value, 0x0102);
+		value = memBuf.getShort(3);
+		assertEquals(value, 0x0304);
+		value = memBuf.getShort(8);
+		assertEquals(value, 0x0809);
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 5, 4);
+		value = memBuf.getShort(0);
+		assertEquals(value, 0x0405);
+		value = memBuf.getShort(1);
+		assertEquals(value, 0x0506);
+		value = memBuf.getShort(3);
+		assertEquals(value, 0x0708);
+		value = memBuf.getShort(8);
+		assertEquals(value, 0x0C0D);
+	}
+
+	@Test
+	public void testGetLongBuffered() throws Exception {
+		loadProgram("notepad");
+		long value;
+
+		Address minAddr = program.getMinAddress();
+		Address maxAddr = program.getMaxAddress();
+		setBytes(minAddr, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 7, 0);
+
+		value = memBuf.getLong(0);
+		assertEquals(value, 0x0001020304050607L);
+		value = memBuf.getLong(1);
+		assertEquals(value, 0x0102030405060708L);
+		value = memBuf.getLong(3);
+		assertEquals(value, 0x030405060708090AL);
+		value = memBuf.getLong(8);
+		assertEquals(value, 0x08090A0B0C0D0E0FL);
+
+		memBuf = new DumbMemBufferImpl(program.getMemory(), minAddr);
+		memBuf = new WrappedMemBuffer(memBuf, 9, 4);
+		value = memBuf.getLong(0);
+		assertEquals(value, 0x0405060708090A0BL);
+		value = memBuf.getLong(1);
+		assertEquals(value, 0x05060708090A0B0CL);
+		value = memBuf.getLong(3);
+		assertEquals(value, 0x0708090A0B0C0D0EL);
+		value = memBuf.getLong(0);
+		assertEquals(value, 0x0405060708090A0BL);
 	}
 
 	private void setBytes(Address addr, byte[] bytes) throws MemoryAccessException {
