99f54346c91ec57ef6ec8a790d046ad5d9a1c415
ghidra1 <ghidra1@users.noreply.github.com>
2021-01-28 09:43:43 -0500

    Corrected float/double convert test

--- Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/equate/EquatePlugin1Test.java
+++ Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/equate/EquatePlugin1Test.java
@@ -904,11 +904,11 @@ public class EquatePlugin1Test extends AbstractEquatePluginTest {
 			}
 			else if (name.indexOf("Double") >= 0) {
 				assertTrue(popupPath[1].startsWith("Double"));
-				assertTrue(popupPath[1].endsWith(" 1.112536929253602E-308"));
+				assertTrue(popupPath[1].endsWith(" 1.976262583364986E-323"));
 			}
 			else if (name.indexOf("Float") >= 0) {
 				assertTrue(popupPath[1].startsWith("Float"));
-				assertTrue(popupPath[1].endsWith(" 5.877475E-39"));
+				assertTrue(popupPath[1].endsWith(" 5.605194E-45"));
 			}
 			else {
 				fail("Unhandled Convert item: " + name);
