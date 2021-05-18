1c52410ad086314908351d6d49d4ea9aa813c03a
jmlagor <73651947+jmlagor@users.noreply.github.com>
2021-05-18 15:25:39 -0400

    GP-955 Check machine arch to stop processing .PDATA section

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/pe/NTHeader.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/pe/NTHeader.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -270,7 +270,9 @@ public class NTHeader implements StructConverter, OffsetValidator {
 
 		fileHeader.processSections(optionalHeader);
 		fileHeader.processSymbols();
-		fileHeader.processImageRuntimeFunctionEntries();
+		if (fileHeader.getMachine() == FileHeader.IMAGE_FILE_MACHINE_AMD64) {
+			fileHeader.processImageRuntimeFunctionEntries();
+		}
 
 		if (advancedProcess) {
 			optionalHeader.processDataDirectories(TaskMonitorAdapter.DUMMY_MONITOR);
