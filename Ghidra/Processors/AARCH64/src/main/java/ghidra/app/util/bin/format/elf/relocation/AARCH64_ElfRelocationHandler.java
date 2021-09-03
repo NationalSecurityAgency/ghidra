eab50936fd919b61c915902be48d891c59005ce8
ghidra1 <ghidra1@users.noreply.github.com>
2021-09-03 09:35:49 -0400

    GP-1278 added support for additional ELF AARCH64 relocations

--- Ghidra/Processors/AARCH64/src/main/java/ghidra/app/util/bin/format/elf/relocation/AARCH64_ElfRelocationHandler.java
+++ Ghidra/Processors/AARCH64/src/main/java/ghidra/app/util/bin/format/elf/relocation/AARCH64_ElfRelocationHandler.java
@@ -167,10 +167,44 @@ public class AARCH64_ElfRelocationHandler extends ElfRelocationHandler {
 				break;
 			}
 
+			// LD/ST16: (S+A) & 0xffe 
+			case AARCH64_ElfRelocationConstants.R_AARCH64_LDST16_ABS_LO12_NC: {
+				int oldValue = memory.getInt(relocationAddress, isBigEndianInstructions);
+				newValue = (int) ((symbolValue + addend) & 0xffe) >> 1;
+
+				newValue = oldValue | (newValue << 10);
+
+				memory.setInt(relocationAddress, (int) newValue, isBigEndianInstructions);
+				break;
+			}
+
 			// LD/ST32: (S+A) & 0xffc
 			case AARCH64_ElfRelocationConstants.R_AARCH64_LDST32_ABS_LO12_NC: {
 				int oldValue = memory.getInt(relocationAddress, isBigEndianInstructions);
 				newValue = (int) ((symbolValue + addend) & 0xffc) >> 2;
+
+				newValue = oldValue | (newValue << 10);
+
+				memory.setInt(relocationAddress, (int) newValue, isBigEndianInstructions);
+				break;
+			}
+
+			// LD/ST64: (S+A) & 0xff8
+			case AARCH64_ElfRelocationConstants.R_AARCH64_LDST64_ABS_LO12_NC:
+			case AARCH64_ElfRelocationConstants.R_AARCH64_LD64_GOT_LO12_NC: {
+				int oldValue = memory.getInt(relocationAddress, isBigEndianInstructions);
+				newValue = (int) ((symbolValue + addend) & 0xff8) >> 3;
+
+				newValue = oldValue | (newValue << 10);
+
+				memory.setInt(relocationAddress, (int) newValue, isBigEndianInstructions);
+				break;
+			}
+
+			// LD/ST128: (S+A) & 0xff0
+			case AARCH64_ElfRelocationConstants.R_AARCH64_LDST128_ABS_LO12_NC: {
+				int oldValue = memory.getInt(relocationAddress, isBigEndianInstructions);
+				newValue = (int) ((symbolValue + addend) & 0xff0) >> 4;
 
 				newValue = oldValue | (newValue << 10);
 
