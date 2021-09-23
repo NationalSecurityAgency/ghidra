932813cd879930b24acb090353e79a2381d815e0
d-millar <33498836+d-millar@users.noreply.github.com>
2021-09-23 17:02:29 +0000

    GP-1288: per request, removed duplicate DummyProc's

--- Ghidra/Debug/Debugger-agent-dbgeng/src/test/java/agent/dbgeng/dbgeng/DbgEngTest.java
+++ Ghidra/Debug/Debugger-agent-dbgeng/src/test/java/agent/dbgeng/dbgeng/DbgEngTest.java
@@ -16,7 +16,7 @@
 package agent.dbgeng.dbgeng;
 
 import static org.junit.Assert.*;
-import static org.junit.Assume.assumeTrue;
+import static org.junit.Assume.*;
 
 import java.io.*;
 import java.nio.ByteBuffer;
@@ -29,7 +29,7 @@ import com.sun.jna.Native;
 import com.sun.jna.platform.win32.COM.COMException;
 import com.sun.jna.win32.StdCallLibrary;
 
-import agent.dbgeng.DummyProc;
+//import agent.dbgeng.DummyProc;
 import agent.dbgeng.dbgeng.DebugBreakpoint.BreakType;
 import agent.dbgeng.dbgeng.DebugClient.*;
 import agent.dbgeng.dbgeng.DebugDataSpaces.*;
@@ -40,6 +40,7 @@ import agent.dbgeng.dbgeng.DebugValue.DebugInt64Value;
 import agent.dbgeng.dbgeng.DebugValue.DebugValueType;
 import agent.dbgeng.dbgeng.util.DebugEventCallbacksAdapter;
 import ghidra.comm.util.BitmaskSet;
+import ghidra.dbg.testutil.DummyProc;
 import ghidra.framework.Application;
 import ghidra.test.AbstractGhidraHeadlessIntegrationTest;
 import ghidra.util.Msg;
@@ -697,7 +698,7 @@ public class DbgEngTest extends AbstractGhidraHeadlessIntegrationTest {
 				return DebugStatus.NO_CHANGE;
 			}
 		});
-		try (DummyProc proc = new DummyProc(specimen)) {
+		try (DummyProc proc = DummyProc.run(specimen)) {
 			System.out.println("Started " + specimen + " with PID=" + proc.pid);
 			Thread.sleep(1000);
 			System.out.println("Attaching...");
@@ -839,7 +840,7 @@ public class DbgEngTest extends AbstractGhidraHeadlessIntegrationTest {
 		final String specimenA = "C:\\windows\\notepad.exe";
 		final String specimenC = "C:\\windows\\system32\\win32calc.exe";
 
-		try (DummyProc proc = new DummyProc(specimenX)) {
+		try (DummyProc proc = DummyProc.run(specimenX)) {
 			client.setOutputCallbacks(new ConsoleOutputCallbacks());
 			BreakAllCallbacks cb = new BreakAllCallbacks(client);
 			client.setEventCallbacks(cb);
