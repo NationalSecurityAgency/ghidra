ae3577a52cc5d998043a69b40976923b071c7663
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2022-01-21 11:42:57 -0500

    GP-1689 - Fixed options names that were not title case

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/analysis/DWARFAnalyzer.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/analysis/DWARFAnalyzer.java
@@ -36,45 +36,52 @@ import ghidra.util.task.TaskMonitor;
 public class DWARFAnalyzer extends AbstractAnalyzer {
 	private static final String DWARF_LOADED_OPTION_NAME = "DWARF Loaded";
 
-	private static final String OPTION_IMPORT_DATATYPES = "Import data types";
+	private static final String OPTION_IMPORT_DATATYPES = "Import Data Types";
 	private static final String OPTION_IMPORT_DATATYPES_DESC =
 		"Import data types defined in the DWARF debug info.";
 
-	private static final String OPTION_PRELOAD_ALL_DIES = "Preload all DIEs";
+	private static final String OPTION_PRELOAD_ALL_DIES = "Preload All DIEs";
 	private static final String OPTION_PRELOAD_ALL_DIES_DESC =
-		"Preload all DIE records. Requires more memory, but necessary for some non-standard layouts.";
+		"Preload all DIE records. Requires more memory, but necessary for some non-standard " +
+			"layouts.";
 
-	private static final String OPTION_IMPORT_FUNCS = "Import functions";
+	private static final String OPTION_IMPORT_FUNCS = "Import Functions";
 	private static final String OPTION_IMPORT_FUNCS_DESC =
-		"Import function information defined in the DWARF debug info.  (implies import data types)";
+		"Import function information defined in the DWARF debug info\n" +
+			"(implies 'Import Data Types' is selected).";
 
-	private static final String OPTION_IMPORT_LIMIT_DIE_COUNT = "Debug item count limit";
+	private static final String OPTION_IMPORT_LIMIT_DIE_COUNT = "Debug Item Limit";
 	private static final String OPTION_IMPORT_LIMIT_DIE_COUNT_DESC =
-		"If the number of DWARF debug items are greater than this setting, DWARF analysis will be skipped.";
+		"If the number of DWARF debug items are greater than this setting, DWARF analysis will " +
+			"be skipped.";
 
-	private static final String OPTION_OUTPUT_SOURCE_INFO = "Output Source info";
+	private static final String OPTION_OUTPUT_SOURCE_INFO = "Output Source Info";
 	private static final String OPTION_OUTPUT_SOURCE_INFO_DESC =
-		"Include source code location info (filename:linenumber) in comments attached to the Ghidra datatype or function or variable created.";
+		"Include source code location info (filename:linenumber) in comments attached to the " +
+			"Ghidra datatype or function or variable created.";
 
-	private static final String OPTION_OUTPUT_DWARF_DIE_INFO = "Output DWARF DIE info";
+	private static final String OPTION_OUTPUT_DWARF_DIE_INFO = "Output DWARF DIE Info";
 	private static final String OPTION_OUTPUT_DWARF_DIE_INFO_DESC =
-		"Include DWARF DIE offset info in comments attached to the Ghidra datatype or function or variable created.";
+		"Include DWARF DIE offset info in comments attached to the Ghidra datatype or function " +
+			"or variable created.";
 
-	private static final String OPTION_NAME_LENGTH_CUTOFF = "Name length cutoff";
+	private static final String OPTION_NAME_LENGTH_CUTOFF = "Maximum Name Length";
 	private static final String OPTION_NAME_LENGTH_CUTOFF_DESC =
 		"Truncate symbol and type names longer than this limit.  Range 20..2000";
 
-	private static final String OPTION_OUTPUT_LEXICAL_BLOCK_COMMENTS = "Lexical block comments";
+	private static final String OPTION_OUTPUT_LEXICAL_BLOCK_COMMENTS = "Add Lexical Block Comments";
 	private static final String OPTION_OUTPUT_LEXICAL_BLOCK_COMMENTS_DESC =
 		"Add comments to the start of lexical blocks";
 
-	private static final String OPTION_OUTPUT_INLINE_FUNC_COMMENTS = "Inlined functions comments";
+	private static final String OPTION_OUTPUT_INLINE_FUNC_COMMENTS =
+		"Add Inlined Functions Comments";
 	private static final String OPTION_OUTPUT_INLINE_FUNC_COMMENTS_DESC =
 		"Add comments to the start of inlined functions";
 
-	private static final String OPTION_OUTPUT_FUNC_SIGS = "Output function signatures";
+	private static final String OPTION_OUTPUT_FUNC_SIGS = "Create Function Signatures";
 	private static final String OPTION_OUTPUT_FUNC_SIGS_DESC =
-		"Create function signature data types for each function encountered in the DWARF debug data.";
+		"Create function signature data types for each function encountered in the DWARF debug " +
+			"data.";
 
 	private static final String DWARF_ANALYZER_NAME = "DWARF";
 	private static final String DWARF_ANALYZER_DESCRIPTION =
