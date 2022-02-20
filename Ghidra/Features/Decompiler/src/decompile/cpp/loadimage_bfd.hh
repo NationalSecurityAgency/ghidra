fd1463b20c825fde8e36affb8db29b32cfd89171
jstaursky <jstaursky@gmail.com>
2022-02-20 11:49:33 -0500

    add macro to loadimage_bfd.hh, easier to build.
    Prevents potential bfd compile issue: "error: config.h must be included before this header"

--- Ghidra/Features/Decompiler/src/decompile/cpp/loadimage_bfd.hh
+++ Ghidra/Features/Decompiler/src/decompile/cpp/loadimage_bfd.hh
@@ -20,7 +20,25 @@
 #define __LOADIMAGE_BFD__
 
 #include "loadimage.hh"
-#include <bfd.h>
+#ifndef PACKAGE
+  #define PACKAGE
+  #ifndef PACKAGE_VERSION
+    #define PACKAGE_VERSION
+    #include <bfd.h>
+    #undef PACKAGE_VERSION
+  #else
+    #include <bfd.h>
+  #endif
+  #undef PACKAGE
+#else
+  #ifndef PACKAGE_VERSION
+    #define PACKAGE_VERSION
+    #include <bfd.h>
+    #undef PACKAGE_VERSION
+  #else
+    #include <bfd.h>
+  #endif
+#endif
 
 struct ImportRecord {
   string dllname;
