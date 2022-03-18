6e733d9fe12e95e767c405b44a4dea7c070f453a
GameCubeGBA <95232096+GameCubeGBA@users.noreply.github.com>
2022-03-18 13:42:47 -0400

    Fix setMaximum so that it actually sets the maximum instead of initializing the task monitors

--- Ghidra/Framework/Docking/src/main/java/ghidra/util/task/CachingSwingWorker.java
+++ Ghidra/Framework/Docking/src/main/java/ghidra/util/task/CachingSwingWorker.java
@@ -288,8 +288,8 @@ public abstract class CachingSwingWorker<T> implements CachingLoader<T> {
 				return;
 			}
 
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).setMessage(message);
+			for (TaskMonitor tm : monitors) {
+				tm.setMessage(message);
 			}
 		}
 
@@ -310,36 +310,36 @@ public abstract class CachingSwingWorker<T> implements CachingLoader<T> {
 
 		@Override
 		public void initialize(long maximum) {
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).initialize(maximum);
+			for (TaskMonitor tm : monitors) {
+				tm.initialize(maximum);
 			}
 		}
 
 		@Override
 		public void setMaximum(long max) {
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).initialize(max);
+			for (TaskMonitor tm : monitors) {
+				tm.setMaximum(max);
 			}
 		}
 
 		@Override
 		public void setProgress(long value) {
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).setProgress(value);
+			for (TaskMonitor tm : monitors) {
+				tm.setProgress(value);
 			}
 		}
 
 		@Override
 		public void incrementProgress(long incrementAmount) {
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).incrementProgress(incrementAmount);
+			for (TaskMonitor tm : monitors) {
+				tm.incrementProgress(incrementAmount);
 			}
 		}
 
 		@Override
 		public void checkCanceled() throws CancelledException {
-			for (int i = 0; i < monitors.size(); i++) {
-				monitors.get(i).checkCanceled();
+			for (TaskMonitor tm : monitors) {
+				tm.checkCanceled();
 			}
 		}
 
@@ -367,8 +367,7 @@ public abstract class CachingSwingWorker<T> implements CachingLoader<T> {
 
 		@Override
 		public boolean isCancelled() {
-			for (int i = 0; i < monitors.size(); i++) {
-				TaskMonitor tm = monitors.get(i);
+			for (TaskMonitor tm : monitors) {
 				if (tm.isCancelled()) {
 					return true;
 				}
