298704348d2fb47d85b32158308b3a7bc1cbc0d7
ghidravore <ghidravore@users.noreply.github.com>
2022-03-30 13:19:59 -0400

    GP-1870 Adding simple string property capability to program user data

--- Ghidra/Framework/Project/src/main/java/ghidra/framework/data/DomainObjectAdapterDB.java
+++ Ghidra/Framework/Project/src/main/java/ghidra/framework/data/DomainObjectAdapterDB.java
@@ -604,6 +604,7 @@ public abstract class DomainObjectAdapterDB extends DomainObjectAdapter
 		DomainObjectAdapterDB userData = getUserData();
 		if (userData != null && userData.isChanged() && (getDomainFile() instanceof GhidraFile)) {
 			try {
+				userData.prepareToSave();
 				userData.save(null, TaskMonitorAdapter.DUMMY_MONITOR);
 			}
 			catch (CancelledException e) {
