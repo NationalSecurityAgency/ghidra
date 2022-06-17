6f8cd7eaa002a40307a046f41a58b8b6914f3e88
alex-bellon <alexrbellon@gmail.com>
2022-06-17 16:30:00 -0700

    Fixed comments for OperandType flags

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/OperandType.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/OperandType.java
@@ -59,7 +59,7 @@ public final class OperandType {
 	public final static int DATA = 0x00000080;
 
 	/**
-	 * Bit set if the operand is a register.
+	 * Bit set if the operand is a port.
 	 */
 	public final static int PORT = 0x00000100;
 	/**
@@ -67,7 +67,7 @@ public final class OperandType {
 	 */
 	public final static int REGISTER = 0x00000200;
 	/**
-	 * Bit set if the operand is a register.
+	 * Bit set if the operand is a list.
 	 */
 	public final static int LIST = 0x00000400;
 	/**
