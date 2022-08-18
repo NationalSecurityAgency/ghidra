555d63683eaf0d20a628cd7fea491007744a6568
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2022-08-18 09:46:32 -0400

    GP-2405 - Small tweaks to the 'Replace Date Type' action to clarify its functionality

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/datamgr/actions/ReplaceDataTypeAction.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/datamgr/actions/ReplaceDataTypeAction.java
@@ -15,11 +15,13 @@
  */
 package ghidra.app.plugin.core.datamgr.actions;
 
+import javax.swing.*;
 import javax.swing.tree.TreePath;
 
 import docking.ActionContext;
 import docking.action.DockingAction;
 import docking.action.MenuData;
+import docking.widgets.label.GHtmlLabel;
 import docking.widgets.tree.GTree;
 import docking.widgets.tree.GTreeNode;
 import ghidra.app.plugin.core.datamgr.DataTypeManagerPlugin;
@@ -28,10 +30,12 @@ import ghidra.app.plugin.core.datamgr.archive.Archive;
 import ghidra.app.plugin.core.datamgr.archive.DataTypeManagerHandler;
 import ghidra.app.plugin.core.datamgr.tree.*;
 import ghidra.app.util.datatype.DataTypeSelectionDialog;
+import ghidra.app.util.datatype.DataTypeSelectionEditor;
 import ghidra.framework.plugintool.PluginTool;
 import ghidra.program.model.data.*;
 import ghidra.util.Msg;
 import ghidra.util.data.DataTypeParser.AllowedDataTypes;
+import ghidra.util.layout.VerticalLayout;
 
 /**
  * Replace the selected data type with the chosen data type
@@ -41,10 +45,10 @@ public class ReplaceDataTypeAction extends DockingAction {
 	private DataTypeManagerPlugin plugin;
 
 	public ReplaceDataTypeAction(DataTypeManagerPlugin plugin) {
-		super("Replace Data Type", plugin.getName());
+		super("Replace", plugin.getName());
 
 		this.plugin = plugin;
-		setPopupMenuData(new MenuData(new String[] { "Replace Data Type..." }, "Edit"));
+		setPopupMenuData(new MenuData(new String[] { "Replace..." }, "Edit"));
 	}
 
 	@Override
@@ -91,24 +95,47 @@ public class ReplaceDataTypeAction extends DockingAction {
 	@Override
 	public void actionPerformed(ActionContext context) {
 
+		DataTypeTreeNode node = getSelectedDataTypeTreeNode(context);
+		String name = node.getName();
+
 		PluginTool tool = plugin.getTool();
 		int noSizeRestriction = -1;
 		DataTypeSelectionDialog selectionDialog = new DataTypeSelectionDialog(tool,
-			plugin.getProgram().getDataTypeManager(), noSizeRestriction, AllowedDataTypes.ALL);
+			plugin.getProgram().getDataTypeManager(), noSizeRestriction, AllowedDataTypes.ALL) {
+
+			@Override
+			protected JComponent createEditorPanel(DataTypeSelectionEditor dtEditor) {
+
+				setTitle("Replace '" + name + "'");
+
+				JPanel updatedPanel = new JPanel();
+				updatedPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 0));
+				updatedPanel.setLayout(new VerticalLayout(5));
+
+				GHtmlLabel label = new GHtmlLabel("<html>Choose the relacment data type: ");
+				label.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+				updatedPanel.add(label);
+
+				updatedPanel.add(dtEditor.getEditorComponent());
+
+				return updatedPanel;
+			}
+
+		};
+		selectionDialog.setHelpLocation(getHelpLocation());
 		tool.showDialog(selectionDialog);
 		DataType newDt = selectionDialog.getUserChosenDataType();
 		if (newDt == null) {
 			return; // cancelled
 		}
 
-		DataTypeTreeNode node = getSelectedDataTypeTreeNode(context);
-
 		DataTypeManagerHandler dtmHandler = plugin.getDataTypeManagerHandler();
-		DataTypeManager dtm = newDt.getDataTypeManager();
-		Archive sourceArchive = dtmHandler.getArchive(dtm);
+		DataTypeManager newDtm = newDt.getDataTypeManager();
+		Archive sourceArchive = dtmHandler.getArchive(newDtm);
 		Archive destinationArchive = findArchive(node);
 
 		DataType oldDt = ((DataTypeNode) node).getDataType();
+		DataTypeManager dtm = oldDt.getDataTypeManager();
 		if (sourceArchive != destinationArchive) {
 			oldDt = oldDt.clone(oldDt.getDataTypeManager());
 		}
