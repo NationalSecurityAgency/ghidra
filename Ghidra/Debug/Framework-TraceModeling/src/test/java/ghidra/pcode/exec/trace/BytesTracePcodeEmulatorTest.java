acd80575c156f03403e1cf693d070d8e1362b565
ghidra1 <ghidra1@users.noreply.github.com>
2023-01-09 09:57:22 -0500

    GP-0 corrected test failure

--- Ghidra/Debug/Framework-TraceModeling/src/test/java/ghidra/pcode/exec/trace/BytesTracePcodeEmulatorTest.java
+++ Ghidra/Debug/Framework-TraceModeling/src/test/java/ghidra/pcode/exec/trace/BytesTracePcodeEmulatorTest.java
@@ -327,7 +327,7 @@ public class BytesTracePcodeEmulatorTest extends AbstractTracePcodeEmulatorTest 
 					""",
 				List.of(
 					// First instruction undoes the modification above
-					"XOR byte ptr [RIP], 0xcc",  // 7 bytes
+					"XOR byte ptr [RIP + 0x0], 0xcc",  // 7 bytes
 					"MOV EAX,0xdeadbeef"));            // 5 bytes
 
 			BytesTracePcodeEmulator emu = new BytesTracePcodeEmulator(tb.host, 0);
