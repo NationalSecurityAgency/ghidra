c099ad70772f852c31d9cca039696e253039f3e4
dev747368 <48332326+dev747368@users.noreply.github.com>
2023-03-14 14:15:23 -0400

    GP-3166 fix Scalar bit twiddling issue with values > 32 bits Main problem was Scalar.getSignedValue() for something created with more than 32 bits and less than 64. Removed unused mutator methods.

--- Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/program/model/scalar/ScalarTest.java
+++ Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/program/model/scalar/ScalarTest.java
@@ -15,15 +15,30 @@
  */
 package ghidra.program.model.scalar;
 
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import generic.test.AbstractGenericTest;
+import ghidra.util.NumericUtilities;
 
 public class ScalarTest extends AbstractGenericTest {
 
-	public ScalarTest() {
-		super();
+	@Test
+	public void testScalar0bits() {
+		// test special allowance for 0-bitlength scalars that have a 0 value
+		assertEquals(0, new Scalar(0, 0).getUnsignedValue());
+		assertEquals(0, new Scalar(0, 0).getSignedValue());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testScalar0bitsFailure() {
+		// test special allowance for 0-bitlength scalars that don't have a 0 value
+		new Scalar(0, 1 /* any non-zero intial value */);
+		fail("Scalar ctor should fail when asked to create a 0 bitLength scalar that isn't 0 value");
 	}
 
 	@Test
@@ -137,4 +152,75 @@ public class ScalarTest extends AbstractGenericTest {
 		Assert.assertEquals(Byte.MAX_VALUE, s.getUnsignedValue());
 
 	}
+
+	@Test
+	public void testIgnoreExtraBits() {
+		// tests that extra bits in the long initial value are ignored when creating value
+		assertEquals(0x22, new Scalar(8, 0x1122).getValue());
+		assertEquals(0x1122, new Scalar(16, 0x1122).getValue());
+	}
+
+	@Test
+	public void testLeftShiftByMoreThan32() {
+		// tests that the impl doesn't mess up when dealing with masks / bits that are larger
+		// than what can be held in a 32bit int.  (eg. 1 << 33 is an error, needs to be 1L << 33)
+		assertEquals(55, new Scalar(32, 55).getSignedValue());
+		assertEquals(55, new Scalar(33, 55).getSignedValue()); // would fail if shift-by-32 error
+
+		assertEquals(0x8000, new Scalar(32, 0x8000).getSignedValue());
+		assertEquals(0x8000, new Scalar(48, 0x8000).getSignedValue()); // would fail if shift-by-32 error
+
+		assertEquals(0x40000000L, new Scalar(32, 0x40000000L).getSignedValue());
+		assertEquals(0x40000000L, new Scalar(63, 0x40000000L).getSignedValue()); // would fail
+
+		assertFalse(new Scalar(64, 0x1_0000_0000L).testBit(31));
+		assertTrue(new Scalar(64, 0x1_0000_0000L).testBit(32));
+		assertFalse(new Scalar(64, 0x1_0000_0000L).testBit(33));
+	}
+
+	@Test
+	public void testGetBigInt() {
+		assertEquals(BigInteger.valueOf(-1), new Scalar(8, 0xffL).getBigInteger());
+		assertEquals(BigInteger.valueOf(-1), new Scalar(32, 0xffffffffL).getBigInteger());
+		assertEquals(BigInteger.valueOf(-1), new Scalar(64, -1L).getBigInteger());
+
+		assertEquals(BigInteger.valueOf(Byte.MIN_VALUE), new Scalar(8, 0x80L).getBigInteger());
+		assertEquals(BigInteger.valueOf(Short.MIN_VALUE), new Scalar(16, 0x8000L).getBigInteger());
+		assertEquals(BigInteger.valueOf(Integer.MIN_VALUE),
+			new Scalar(32, 0x80000000L).getBigInteger());
+		assertEquals(BigInteger.valueOf(Long.MIN_VALUE),
+			new Scalar(64, Long.MIN_VALUE).getBigInteger());
+
+		assertEquals(BigInteger.valueOf(0x80L), new Scalar(9, 0x80L).getBigInteger());
+		assertEquals(BigInteger.valueOf(0x8000L), new Scalar(17, 0x8000L).getBigInteger());
+		assertEquals(BigInteger.valueOf(0x8000_0000L),
+			new Scalar(33, 0x8000_0000L).getBigInteger());
+		assertEquals(BigInteger.valueOf(0x1_8000_0000L),
+			new Scalar(34, 0x1_8000_0000L).getBigInteger());
+	}
+
+	@Test
+	public void testTestBit() {
+		Scalar scalar = new Scalar(64, -1, true);
+		for (int bitnum = 0; bitnum < 64; bitnum++) {
+			assertTrue(scalar.testBit(bitnum));
+		}
+
+		// test top half of int64 are 0's, bottom half are 1's
+		scalar = new Scalar(64, NumericUtilities.MAX_UNSIGNED_INT32_AS_LONG);
+		for (int bitnum = 0; bitnum < 32; bitnum++) {
+			assertTrue(scalar.testBit(bitnum));
+		}
+		for (int bitnum = 32; bitnum < 64; bitnum++) {
+			assertFalse(scalar.testBit(bitnum));
+		}
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testScalar0bitsTestBit() {
+		Scalar scalar = new Scalar(0, 0);
+		scalar.testBit(0);
+		fail("Scalar testBit(0) should fail");
+	}
+
 }
