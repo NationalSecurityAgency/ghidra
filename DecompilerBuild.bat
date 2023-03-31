a860971f694730c8e99a40cccc9cc34814f65d27
Wall-AF <Allan.F.Wall@users.noreply.github.com>
2023-03-31 09:36:43 +0100

    Removed unnecessary files

--- DecompilerBuild.bat
+++
@@ -1,3 +0,0 @@
-dir Ghidra\Features\Decompiler\build\os\win_x86_64
-CALL gradle decompileWin_x86_64Executable
-dir Ghidra\Features\Decompiler\build\os\win_x86_64
--- sleighRebuild.bat
+++
@@ -1,3 +0,0 @@
-@SET /p CHOICE=Clean Sleigh?
-@IF '%CHOICE%'=='y' CALL gradle cleanSleigh
-CALL gradle x86:sleighCompile
