8b82f13e0f5388b6497b6b4b9a7a6a0f0d83be91
Luke Serné <luke.serne@solconmail.nl>
2023-08-25 17:03:21 +0200

    Decompiler: Add optimisation for LUTs encoded in constants gcc 13 and clang 16 at -O1 or higher compile an "||" sequence of comparisons of a single variable with various small values to a single right shift of a magic constant functioning as a lookup table (LUT) by a variable amount and a binary and with 1. This rule replaces this magic constant by a series of integer comparisons, which leads to better decompilation, especially if enums are involved.

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -5450,6 +5450,7 @@ void ActionDatabase::universalAction(Architecture *conf)
 	actprop->addRule( new RuleOrMultiBool("analysis") );
 	actprop->addRule( new RuleXorSwap("analysis") );
 	actprop->addRule( new RuleLzcountShiftBool("analysis") );
+	actprop->addRule( new RuleSimplifyConstantLUT("analysis") );
 	actprop->addRule( new RuleSubvarAnd("subvar") );
 	actprop->addRule( new RuleSubvarSubpiece("subvar") );
 	actprop->addRule( new RuleSplitFlow("subvar") );
