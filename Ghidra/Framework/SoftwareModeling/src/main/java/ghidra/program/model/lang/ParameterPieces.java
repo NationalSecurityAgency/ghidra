99d8762b9cf3cdf8ad2db94673a94082f452837d
caheckman <48068198+caheckman@users.noreply.github.com>
2023-12-11 22:38:21 +0000

    GP-4133 Adjustment for forced indirect return

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/ParameterPieces.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/ParameterPieces.java
@@ -92,18 +92,15 @@ public class ParameterPieces {
 		VariableStorage store;
 		try {
 			if (joinPieces != null) {
-				store = new DynamicVariableStorage(program, false, joinPieces);
+				store = new DynamicVariableStorage(program, isIndirect, joinPieces);
 			}
 			else {
 				if (hiddenReturnPtr) {
 					store = new DynamicVariableStorage(program,
 						AutoParameterType.RETURN_STORAGE_PTR, address, sz);
 				}
-				else if (isIndirect) {
-					store = new DynamicVariableStorage(program, true, address, sz);
-				}
 				else {
-					store = new DynamicVariableStorage(program, false, address, sz);
+					store = new DynamicVariableStorage(program, isIndirect, address, sz);
 				}
 			}
 		}
