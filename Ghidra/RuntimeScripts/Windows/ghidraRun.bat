3588903a8ad64da3db5cea046b124ff971296fd1
Taylor Christian Newsome <86472964+SleepTheGod@users.noreply.github.com>
2023-12-20 18:45:13 -0500

    Update ghidraRun.bat

--- Ghidra/RuntimeScripts/Windows/ghidraRun.bat
+++ Ghidra/RuntimeScripts/Windows/ghidraRun.bat
@@ -1,11 +1,17 @@
-:: Ghidra launch
-
 @echo off
 setlocal
 
-:: Maximum heap memory may be changed if default is inadequate. This will generally be up to 1/4 of 
-:: the physical memory available to the OS. Uncomment MAXMEM setting if non-default value is needed.
-::set MAXMEM=2G
+REM Maximum heap memory calculation based on available system resources
+for /f "tokens=2 delims==" %%I in ('wmic OS | findstr /i "TotalVisibleMemorySize"') do set "TotalMemory=%%I"
+set /a "MAXMEM=TotalMemory / 4194304"  REM Allocating 1/4th of total memory (in bytes), adjust divisor as needed
 
-call "%~dp0support\launch.bat" bg jdk Ghidra "%MAXMEM%" "" ghidra.GhidraRun %*
+call "%~dp0support\launch.bat" bg jdk Ghidra "%MAXMEM%G" "" ghidra.GhidraRun %*
 
+REM Error handling
+if errorlevel 1 (
+    echo There was an issue launching Ghidra. Please check your configuration and try again.
+    exit /b 1
+)
+
+REM Successful launch
+exit /b 0
