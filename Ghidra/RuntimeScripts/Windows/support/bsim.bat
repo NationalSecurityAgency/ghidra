735cb1a5c3cf476eba88c7914824115d7c202ea3
ghidra1 <ghidra1@users.noreply.github.com>
2024-01-03 17:29:45 -0500

    GP-4173 Complete transition to using --option params with bsim and bsim_ctl commands

--- Ghidra/RuntimeScripts/Windows/support/bsim.bat
+++ Ghidra/RuntimeScripts/Windows/support/bsim.bat
@@ -3,9 +3,8 @@
 @echo off
 setlocal
 
-:: Maximum heap memory may be changed if default is inadequate. This will generally be up to 1/4 of 
-:: the physical memory available to the OS. Uncomment MAXMEM setting if non-default value is needed.
-::set MAXMEM=2G
+:: maximum heap memory (may be increased)
+set MAXMEM=2G
 
 :: launch mode  (fg, bg, debug, debug-suspend)
 set LAUNCH_MODE=fg
