2c69ffb04a8b6f4fce54661e47820a5bc92a06cc
d-millar <33498836+d-millar@users.noreply.github.com>
2024-02-27 10:57:38 -0500

    GP-4290: fix for selectProcess/Thread GP-4290: fix for hard-coded path GP-4290: simpler fixes post-review GP-4290: partial fixes for slow start & termination logic GP-4290: build logic for tlb GP-4290: running dbgmodel locally GP-4290: atttempting to isolate dbgmodel code GP-4209: filling out unimplemented methods GP-4290: options for dbgeng vs dbgmodel GP-4290: aisle 9 for schema GP-4290: Aggregate for Debug.Breakpoints & Stack.Frames GP-4290: hidden attrs for schema GP-4290: working in both modes GP-4290: unified interface (use_generics) GP-4290: basics working - mem, mods, dis, regs GP-4290: somewhat working - model is populated GP-4290: schema updates GP-4290: fix for names GP-4290: AddRef GP-4290: switch to byref GP-4290: start on dbgmodel interposing logic GP-4290: added GetIntrinsicValue GP-4290: moving along GP-4290: first pass

--- Ghidra/Debug/Debugger-agent-dbgeng/src/main/py/src/ghidradbg/__init__.py
+++ Ghidra/Debug/Debugger-agent-dbgeng/src/main/py/src/ghidradbg/__init__.py
@@ -14,7 +14,44 @@
 #  limitations under the License.
 ##
 from . import util, commands, methods, hooks
+from dbgmodel.ihostdatamodelaccess import HostDataModelAccess
 import ctypes
+import platform
+import os
 
 
 ctypes.windll.kernel32.SetErrorMode(0x0001 | 0x0002 | 0x8000)
+
+if platform.architecture()[0] == '64bit':
+    dbgdirs = [os.getenv('OPT_DBGMODEL_PATH'),
+               r'C:\Program Files\Windows Kits\10\Debuggers\x64',
+               r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64']
+else:
+    dbgdirs = [os.getenv('OPT_DBGMODEL_PATH'),
+               r'C:\Program Files\Windows Kits\10\Debuggers\x86',
+               r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x86']
+dbgdir = None
+for _dir in dbgdirs:
+    if os.path.exists(_dir):
+        dbgdir = _dir
+        break
+
+if not dbgdir:
+    raise RuntimeError("Windbg install directory not found!")
+
+# preload these to get correct DLLs loaded
+try:
+    ctypes.windll.LoadLibrary(os.path.join(dbgdir, 'dbghelp.dll'))
+except Exception as exc:
+    print(f"LoadLibrary failed: {dbgdir}\dbghelp.dll {exc}")
+    pass
+try:
+    ctypes.windll.LoadLibrary(os.path.join(dbgdir, 'dbgeng.dll'))
+except Exception as exc:
+    print(f"LoadLibrary failed: {dbgdir}\dbgeng.dll {exc}")
+    pass
+try:
+    ctypes.windll.LoadLibrary(os.path.join(dbgdir, 'DbgModel.dll'))
+except Exception as exc:
+    print(f"LoadLibrary failed: {dbgdir}\dbgmodel.dll {exc}")
+    pass
