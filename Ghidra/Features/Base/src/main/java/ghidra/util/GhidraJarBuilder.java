3fb54aa023f5649340d39c0c5ade392e6ef4d1df
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-04-29 13:57:02 -0400

    GP-0: More GhidraJarBuilder -srczip fixes

--- Ghidra/Features/Base/src/main/java/ghidra/util/GhidraJarBuilder.java
+++ Ghidra/Features/Base/src/main/java/ghidra/util/GhidraJarBuilder.java
@@ -328,7 +328,7 @@ public class GhidraJarBuilder implements GhidraLaunchable {
 		Collections.sort(moduleList);
 
 		for (ApplicationModule module : moduleList) {
-			File srcDir = new File(module.getModuleDir(), "src");
+			File srcDir = new File(module.getModuleDir(), "lib");
 			File srcZipFileForModule = new File(srcDir, module.getName() + "-src.zip");
 			if (srcZipFileForModule.exists()) {
 				writeModuleSrcZipToOverallSrcZip(zip, srcZipFileForModule);
@@ -339,15 +339,16 @@ public class GhidraJarBuilder implements GhidraLaunchable {
 			}
 		}
 		if (!wroteToZip) {
-			System.out.println("Can't create source zip!  Has source been downloaded and installed?");
+			System.out
+					.println("Can't create source zip!  Has source been downloaded and installed?");
 		}
-		// zip.close reports error if nothing has been written to it
 		try {
+			// zip.close may report error if nothing has been written to it
 			zip.close();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 	private void writeModuleSrcZipToOverallSrcZip(Zip zip, File srcZipFileForModule)
