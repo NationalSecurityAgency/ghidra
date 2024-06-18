999004245a345dd53c5e0cc2d2014be69595c455
ghizard <50744617+ghizard@users.noreply.github.com>
2024-06-18 11:45:36 +0000

    GP-4700 - Improve PDB and MDMang developer scripts

--- Ghidra/Features/PDB/developer_scripts/PdbDeveloperDumpMangledTypeNamesScript.java
+++ Ghidra/Features/PDB/developer_scripts/PdbDeveloperDumpMangledTypeNamesScript.java
@@ -25,7 +25,8 @@ import ghidra.app.util.bin.format.pdb2.pdbreader.*;
 import ghidra.app.util.bin.format.pdb2.pdbreader.type.AbstractComplexMsType;
 import ghidra.app.util.bin.format.pdb2.pdbreader.type.AbstractMsType;
 import ghidra.features.base.values.GhidraValuesMap;
-import ghidra.util.*;
+import ghidra.util.MessageType;
+import ghidra.util.StatusListener;
 import ghidra.util.exception.CancelledException;
 
 public class PdbDeveloperDumpMangledTypeNamesScript extends GhidraScript {
@@ -88,6 +89,7 @@ public class PdbDeveloperDumpMangledTypeNamesScript extends GhidraScript {
 		values.setValidator((valueMap, status) -> {
 			return validatePdb(valueMap, status) && validateOutput(valueMap, status);
 		});
+		setReusePreviousChoices(false); // false for second pass... want our default output
 		values = askValues(TITLE, null, values);
 		pdbFile = values.getFile(PDB_PROMPT); // might have changed
 		pdbFileName = pdbFile.getAbsolutePath(); // might have changed
@@ -95,13 +97,13 @@ public class PdbDeveloperDumpMangledTypeNamesScript extends GhidraScript {
 
 		if (dumpFile.exists()) {
 			if (!askYesNo("Confirm Overwrite", "Overwrite file: " + dumpFile.getName())) {
-				Msg.info(this, "Operation canceled");
+				println("Operation canceled");
 				return;
 			}
 		}
 
 		String message = "Processing PDB Dump of: " + pdbFileName;
-		Msg.info(this, message);
+		println(message);
 		try (AbstractPdb pdb = PdbParser.parse(pdbFile, new PdbReaderOptions(), monitor)) {
 			pdb.deserialize();
 			FileWriter fileWriter = new FileWriter(dumpFile);
@@ -110,11 +112,11 @@ public class PdbDeveloperDumpMangledTypeNamesScript extends GhidraScript {
 			bufferedWriter.close();
 		}
 		catch (IOException ioe) {
-			Msg.info(this, ioe.getMessage());
+			println(ioe.getMessage());
 			popup(ioe.getMessage());
 		}
 		message = "Results located in: " + dumpFile.getAbsoluteFile();
-		Msg.info(this, message);
+		println(message);
 	}
 
 	private void dumpMangledTypeNames(AbstractPdb pdb, Writer myWriter)
