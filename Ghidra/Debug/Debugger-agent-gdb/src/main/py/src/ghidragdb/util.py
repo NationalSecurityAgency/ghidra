4a9eec28ccdc2d0cb0ce33486543271ae1e898ac
d-millar <33498836+d-millar@users.noreply.github.com>
2024-07-03 14:44:28 +0000

    GP-4745: catchpoint fixes

--- Ghidra/Debug/Debugger-agent-gdb/src/main/py/src/ghidragdb/util.py
+++ Ghidra/Debug/Debugger-agent-gdb/src/main/py/src/ghidragdb/util.py
@@ -344,6 +344,41 @@ def _choose_breakpoint_location_info_reader():
 BREAKPOINT_LOCATION_INFO_READER = _choose_breakpoint_location_info_reader()
 
 
+class MemCatchpointSetterV8(object):
+    def install_catchpoint(self):
+        return object()
+
+
+class MemCatchpointSetterV11(object):
+    def install_catchpoint(self):
+        breaks_before = set(gdb.breakpoints())
+        try:
+            gdb.execute("""
+                catch syscall group:memory
+                commands
+                silent
+                hooks-ghidra event-memory
+                cont
+                end
+                """)            
+            return (set(gdb.breakpoints()) - breaks_before).pop()
+        except Exception as e:
+            print(f"Error setting memory catchpoint: {e}")
+            return object()
+
+
+def _choose_mem_catchpoint_setter():
+    if GDB_VERSION.major >= 11:
+        return MemCatchpointSetterV11()
+    if GDB_VERSION.major >= 8:
+        return MemCatchpointSetterV8()
+    else:
+        raise gdb.GdbError(
+            "GDB version not recognized by ghidragdb: " + GDB_VERSION.full)
+
+
+MEM_CATCHPOINT_SETTER = _choose_mem_catchpoint_setter()
+
 def set_bool_param_by_api(name, value):
     gdb.set_parameter(name, value)
 
