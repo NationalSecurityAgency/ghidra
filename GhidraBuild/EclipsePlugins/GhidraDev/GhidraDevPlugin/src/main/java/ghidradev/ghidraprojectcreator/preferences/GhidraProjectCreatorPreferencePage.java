dfe5fb992d53540ce4b93e849e0ef50307309705
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-07-29 09:07:07 -0400

    GP-3490: Fixing GhidraDev required versions

--- GhidraBuild/EclipsePlugins/GhidraDev/GhidraDevPlugin/src/main/java/ghidradev/ghidraprojectcreator/preferences/GhidraProjectCreatorPreferencePage.java
+++ GhidraBuild/EclipsePlugins/GhidraDev/GhidraDevPlugin/src/main/java/ghidradev/ghidraprojectcreator/preferences/GhidraProjectCreatorPreferencePage.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,7 @@ import utility.application.ApplicationLayout;
 public class GhidraProjectCreatorPreferencePage extends PreferencePage
 implements IWorkbenchPreferencePage {
 
-	private static ApplicationVersion MIN_GHIDRA_VERSION = new ApplicationVersion("9.1");
+	private static ApplicationVersion MIN_GHIDRA_VERSION = new ApplicationVersion("11.2");
 
 	private Table table;
 	private Button addButton;
@@ -232,7 +232,7 @@ implements IWorkbenchPreferencePage {
 		boolean layoutVersionError = false;
 		try {
 			int ver = Integer.parseInt(layoutVersion);
-			if (ver < 1 || ver > 2) {
+			if (ver < 1 || ver > 3) {
 				layoutVersionError = true;
 			}
 		}
