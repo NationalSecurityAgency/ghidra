8509c3a9e40009434546b5a118b267b347878641
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-08-13 09:16:59 -0400

    GP-4844: Fixed ClassSearcher exception when launching from single jar mode (Closes #6809)

--- Ghidra/Framework/Generic/src/main/java/ghidra/util/classfinder/ClassSearcher.java
+++ Ghidra/Framework/Generic/src/main/java/ghidra/util/classfinder/ClassSearcher.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -176,7 +176,7 @@ public class ClassSearcher {
 		}
 
 		List<Class<? extends T>> list = new ArrayList<>();
-		for (ClassFileInfo info : extensionPointSuffixToInfoMap.get(suffix)) {
+		for (ClassFileInfo info : extensionPointSuffixToInfoMap.getOrDefault(suffix, Set.of())) {
 
 			if (falsePositiveCache.contains(info)) {
 				continue;
