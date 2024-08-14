20f4bee74619cc7855453e3d9dc354b6025776ea
Dan <46821332+nsadeveloper789@users.noreply.github.com>
2024-08-14 11:19:40 -0400

    GP-4788: Correct use of trace base address factory.

--- Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/listing/DBTraceInstructionsMemoryView.java
+++ Ghidra/Debug/Framework-TraceModeling/src/main/java/ghidra/trace/database/listing/DBTraceInstructionsMemoryView.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -75,7 +75,7 @@ public class DBTraceInstructionsMemoryView
 		for (InstructionBlock block : mappedSet) {
 			InstructionSet setPerSpace =
 				breakDown.computeIfAbsent(block.getStartAddress().getAddressSpace(),
-					s -> new InstructionSet(manager.getBaseLanguage().getAddressFactory()));
+					s -> new InstructionSet(manager.getTrace().getBaseAddressFactory()));
 			setPerSpace.addBlock(block);
 		}
 		AddressSet result = new AddressSet();
