08004a3376bb5bba724821e474d0b5dd74a59e7b
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2024-08-27 09:31:53 -0400

    Test fixes

--- Ghidra/Features/Base/src/test/java/ghidra/framework/options/OptionsTest.java
+++ Ghidra/Features/Base/src/test/java/ghidra/framework/options/OptionsTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -665,7 +665,7 @@ public class OptionsTest extends AbstractGuiTest {
 		int value;
 
 		@SuppressWarnings("unused")
-		MyCustomOption() {
+		public MyCustomOption() {
 			// used by reflection
 		}
 
@@ -694,6 +694,11 @@ public class OptionsTest extends AbstractGuiTest {
 		@Override
 		public int hashCode() {
 			return 0;
+		}
+
+		@Override
+		public String toString() {
+			return "MyCustomOption[value=%s]".formatted(value);
 		}
 	}
 
