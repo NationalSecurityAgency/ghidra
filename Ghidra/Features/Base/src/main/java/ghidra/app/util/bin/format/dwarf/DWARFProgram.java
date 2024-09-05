066a883e7903086b30967ee3f64da7b2abbf329e
dev747368 <48332326+dev747368@users.noreply.github.com>
2024-09-05 21:35:08 +0000

    GP-4987 fix DWARF handling of rangelist and locationlists
    The logic was not handling attributes encoded via dw_form_dataN (a
    constant).
    
    Additionally, quieted warnings about mismatched attribute/form
    combinations.
    
    Fixes issue #5982

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/dwarf/DWARFProgram.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/dwarf/DWARFProgram.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -1090,7 +1090,10 @@ public class DWARFProgram implements Closeable {
 				debugRngLists.setPointerIndex(rnglistOffset);
 				return DWARFRangeList.readV5(debugRngLists, cu);
 			}
-			case DW_FORM_sec_offset: {
+			case DW_FORM_sec_offset:
+			case DW_FORM_data2:
+			case DW_FORM_data4:
+			case DW_FORM_data8: {
 				long rnglistOffset = rngListAttr.getValue();
 				short dwarfVersion = cu.getDWARFVersion();
 				if (dwarfVersion < 5) {
@@ -1204,6 +1207,9 @@ public class DWARFProgram implements Closeable {
 		try {
 			switch (loclistAttr.getAttributeForm()) {
 				case DW_FORM_sec_offset:
+				case DW_FORM_data2:
+				case DW_FORM_data4:
+				case DW_FORM_data8:
 					int dwarfVer = cu.getDWARFVersion();
 					if (dwarfVer < 5) {
 						debugLocation.setPointerIndex(loclistAttr.getUnsignedValue());
