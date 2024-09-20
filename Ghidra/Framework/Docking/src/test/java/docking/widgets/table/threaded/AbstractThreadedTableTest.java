6e047c9137519a1f3ec9858e68cc6acf90b539b9
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2024-09-20 16:25:47 -0400

    Test fixes

--- Ghidra/Framework/Docking/src/test/java/docking/widgets/table/threaded/AbstractThreadedTableTest.java
+++ Ghidra/Framework/Docking/src/test/java/docking/widgets/table/threaded/AbstractThreadedTableTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,7 @@ import javax.swing.table.JTableHeader;
 
 import org.junit.*;
 
+import docking.DockingFrame;
 import docking.DockingUtils;
 import docking.test.AbstractDockingTest;
 import docking.widgets.table.*;
@@ -75,7 +76,7 @@ public abstract class AbstractThreadedTableTest extends AbstractDockingTest {
 
 	protected void buildFrame(GThreadedTablePanel<Long> tablePanel) {
 		runSwing(() -> {
-			frame = new JFrame("Threaded Table Test");
+			frame = new DockingFrame("Threaded Table Test");
 			frame.getContentPane().setLayout(new BorderLayout());
 			frame.getContentPane().add(new JScrollPane(tablePanel));
 			frame.pack();
