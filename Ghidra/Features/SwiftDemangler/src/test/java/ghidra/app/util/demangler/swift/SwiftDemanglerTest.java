ce2f8b94cf9c944a489e94fcc5e7b8da2c44971e
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-09-23 11:53:22 -0400

    GP-4898: Fixes for the SwiftDemangler

--- Ghidra/Features/SwiftDemangler/src/test/java/ghidra/app/util/demangler/swift/SwiftDemanglerTest.java
+++ Ghidra/Features/SwiftDemangler/src/test/java/ghidra/app/util/demangler/swift/SwiftDemanglerTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -701,5 +701,11 @@ public class SwiftDemanglerTest extends AbstractGenericTest {
 			throw new AssertException("Demangled object is not a function");
 		}
 		assertEquals(demangled, function.toString());
+	}
+
+	@Test
+	public void testUnsupportedString() throws Exception {
+		String mangled = "__mh_execute_header";
+		assertNull(demangler.demangle(mangled));
 	}
 }
