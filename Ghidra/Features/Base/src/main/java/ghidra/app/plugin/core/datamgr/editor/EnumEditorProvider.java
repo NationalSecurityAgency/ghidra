5d499c990e743d0e1d3c19a31225f9486355e7cd
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2024-10-07 16:40:13 -0400

    Added a help entry for an action missing help

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/datamgr/editor/EnumEditorProvider.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/datamgr/editor/EnumEditorProvider.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -272,6 +272,7 @@ public class EnumEditorProvider extends ComponentProviderAdapter
 
 	private void createActions() {
 		hexDisplayAction = new ToggleActionBuilder("Toggle Hex Mode", plugin.getName())
+				.helpLocation(new HelpLocation(HELP_TOPIC, "Toggle_Hex_Mode"))
 				.menuPath("Show Enum Values in Hex")
 				.description("Toggles Enum value column to show values in hex or decimal")
 				.keyBinding("Shift-H")
