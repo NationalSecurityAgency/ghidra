cdf309cfc750369c3dbb02f70b7609cf30e5f2d7
dev747368 <48332326+dev747368@users.noreply.github.com>
2024-10-28 20:32:10 +0000

    GP-5031 PR #7070 followup, invert '.' dot logic, use ext for txt, jpg
    Invert the file name extension convention from yes-leading-dot to
    no-leading-dot, to match other classes that specify extensions.
    
    Use the filename ext for the txt and image fsbfilehandlers.

--- Ghidra/Features/Base/src/main/java/ghidra/plugins/fsbrowser/FSBFileNode.java
+++ Ghidra/Features/Base/src/main/java/ghidra/plugins/fsbrowser/FSBFileNode.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,6 +86,13 @@ public class FSBFileNode extends FSBNode {
 
 	public String getFilenameExtOverride() {
 		return filenameExtOverride;
+	}
+
+	@Override
+	public String getFileExtension() {
+		return filenameExtOverride != null && !filenameExtOverride.isEmpty()
+				? filenameExtOverride
+				: super.getFileExtension();
 	}
 
 	@Override
