e75a1a8572b56d7194d28d388f580d6ae52e4e28
Michael Pollmeier <michael@michaelpollmeier.com>
2024-11-07 16:29:19 +0100

    sonatype central: update our release process

--- ghidra-publish.sh
+++ ghidra-publish.sh
@@ -27,31 +27,17 @@ support/buildGhidraJar
 # add classes from ByteViewer.jar - those are looked up at runtime via reflection
 # context: lookup happens transitively by loading classes from _Root/Ghidra/EXTENSION_POINT_CLASSES
 unzip Ghidra/Features/ByteViewer/lib/ByteViewer.jar -d byteviewer
-cd byteviewer
-zip -r ../ghidra.jar *
-cd ..
+pushd byteviewer
+  zip -r ../ghidra.jar *
+popd
 
-# hack: remove h2 database from the fat jar - only because we need to use it together with overflowdb which has it's own dependency on a different version of h2... jar hell ftw
-# can be removed once joern is migrated to flatgraph
-zip ghidra.jar -d '*org/h2/*'
+# create a simplistic dummy maven project that will help us release to maven central
+cp ../pom.xml.template pom.xml
+sed -i s/__VERSION__/$CUSTOM_RELEASE_VERSION/ pom.xml
 
-# install into local maven repo, mostly to generate a pom
-mvn install:install-file -DgroupId=io.joern -DartifactId=ghidra -Dpackaging=jar -Dversion=$CUSTOM_RELEASE_VERSION -Dfile=ghidra.jar -DgeneratePom=true
-cp ~/.m2/repository/io/joern/ghidra/$CUSTOM_RELEASE_VERSION/ghidra-$CUSTOM_RELEASE_VERSION.pom pom.xml
+# deploy to sonatype central
+mvn release
 
-# add pom-extra to pom.xml, to make sonatype happy
-head -n -1 pom.xml > pom.tmp
-cat pom.tmp ../pom-extra > pom.xml
-rm pom.tmp
-
-# create empty jar for "sources" - just to make sonatype happy
-zip empty.jar LICENSE
-
-# sign and upload artifacts to sonatype staging
-mvn gpg:sign-and-deploy-file -Durl=$SONATYPE_URL -DrepositoryId=$REPO_ID -DpomFile=pom.xml -Dclassifier=sources -Dfile=empty.jar
-mvn gpg:sign-and-deploy-file -Durl=$SONATYPE_URL -DrepositoryId=$REPO_ID -DpomFile=pom.xml -Dclassifier=javadoc -Dfile=docs/GhidraAPI_javadoc.zip
-mvn gpg:sign-and-deploy-file -Durl=$SONATYPE_URL -DrepositoryId=$REPO_ID -DpomFile=pom.xml -Dfile=ghidra.jar
-
-echo "artifacts are now published to sonatype central. next step: log into https://central.sonatype.com/publishing/deployments and publish it to maven central"
+echo "release is now published to sonatype central. next step: log into https://central.sonatype.com/publishing/deployments and publish it to maven central"
 echo "once it's synchronised to maven central (repo1), update the ghidra version in 'joern/joern-cli/frontends/ghidra2cpg/build.sbt'"
 echo "don't forget to commit and push the local changes in this repo to https://github.com/joernio/ghidra"
