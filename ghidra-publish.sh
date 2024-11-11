07606b78f0a99d8e85e749642b05b0830fa71c84
Michael Pollmeier <michael@michaelpollmeier.com>
2024-11-11 11:59:40 +0100

    fixup release - jar was empty before this

--- ghidra-publish.sh
+++ ghidra-publish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -e #stop on error
 set -o pipefail
 
@@ -10,7 +11,7 @@ set -o pipefail
 VERSION=11.2.1_PUBLIC_20241105
 VERSION_SHORTER=11.2.1
 VERSION_SHORT=${VERSION_SHORTER}_PUBLIC
-CUSTOM_RELEASE_VERSION=${VERSION}-1
+CUSTOM_RELEASE_VERSION=${VERSION}-2
 
 SONATYPE_URL=https://central.sonatype.com/service/local/staging/deploy/maven2/
 # the server id from your local ~/.m2/settings.xml
@@ -24,20 +25,22 @@ rm ghidra_$VERSION.zip
 cd ghidra_${VERSION_SHORT}
 support/buildGhidraJar
 
+# create custom-made maven build just for deploying to maven central
+mkdir -p build/src/main/resources
+sed s/__VERSION__/$CUSTOM_RELEASE_VERSION/ ../pom.xml.template > build/pom.xml
+unzip -d build/src/main/resources ghidra.jar
+
 # add classes from ByteViewer.jar - those are looked up at runtime via reflection
 # context: lookup happens transitively by loading classes from _Root/Ghidra/EXTENSION_POINT_CLASSES
-unzip Ghidra/Features/ByteViewer/lib/ByteViewer.jar -d byteviewer
-pushd byteviewer
-  zip -r ../ghidra.jar *
-popd
+# unzip flag `-o` to override and update files without prompting the user
+unzip -o -d build/src/main/resources Ghidra/Features/ByteViewer/lib/ByteViewer.jar
 
-# create a simplistic dummy maven project that will help us release to maven central
-cp ../pom.xml.template pom.xml
-sed -i s/__VERSION__/$CUSTOM_RELEASE_VERSION/ pom.xml
 
 # deploy to sonatype central
-mvn deploy
+pushd build
+  mvn deploy
+popd
 
 echo "release is now published to sonatype central. next step: log into https://central.sonatype.com/publishing/deployments and publish it to maven central"
-echo "once it's synchronised to maven central (repo1), update the ghidra version in 'joern/project/Versions.scala' to $CUSTOM_RELEASE_VERSION"
+echo "once it's synchronised to maven central (https://repo1.maven.org/maven2/io/joern/ghidra/), update the ghidra version in 'joern/project/Versions.scala' to $CUSTOM_RELEASE_VERSION"
 echo "don't forget to commit and push the local changes in this repo to https://github.com/joernio/ghidra"
