058ecb5eef00f819416d2d1d91cab22a76d3c224
ghidra1 <ghidra1@users.noreply.github.com>
2024-11-18 11:29:53 -0500

    GP-0 Revert GP-4886 change pending further review and rework

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/function/FunctionDB.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/database/function/FunctionDB.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -2468,6 +2468,16 @@ public class FunctionDB extends DatabaseObject implements Function {
 			checkIsValid();
 			if (thunkedFunction != null) {
 				return thunkedFunction.getSignatureSource();
+			}
+
+			// Force DEFAULT source if any param has unassigned storage
+			if (!getReturn().isValid()) {
+				return SourceType.DEFAULT;
+			}
+			for (Parameter param : getParameters()) {
+				if (!param.isValid()) {
+					return SourceType.DEFAULT;
+				}
 			}
 
 			return getStoredSignatureSource();
