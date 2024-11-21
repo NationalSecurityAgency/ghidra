b44ff6e230275fb17c2df986ef0cda193dc1bbff
d-millar <33498836+d-millar@users.noreply.github.com>
2024-11-21 14:39:21 +0000

    GP-0: fix for test failures

--- Ghidra/Test/DebuggerIntegrationTest/src/test.slow/java/agent/lldb/rmi/LldbCommandsTest.java
+++ Ghidra/Test/DebuggerIntegrationTest/src/test.slow/java/agent/lldb/rmi/LldbCommandsTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -243,6 +243,7 @@ public class LldbCommandsTest extends AbstractLldbTraceRmiTest {
 				Selected Ghidra compiler: %s""".formatted(PLAT.lang(), PLAT.cSpec()),
 			extractOutSection(out, "---File---"));
 		assertEquals("""
+				Toy:BE:64:default not found in compiler map
 				Selected Ghidra language: Toy:BE:64:default
 				Selected Ghidra compiler: default""",
 			extractOutSection(out, "---Language---"));
