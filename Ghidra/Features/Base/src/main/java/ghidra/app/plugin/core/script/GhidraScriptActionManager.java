478d3e6331803ee3c4adda98a9a97e0acab7e242
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-12-03 07:03:13 -0500

    GP-5148: More polished Visual Studio Code integration

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/script/GhidraScriptActionManager.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/script/GhidraScriptActionManager.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -225,6 +225,9 @@ class GhidraScriptActionManager {
 
 		createScriptAction("EditEclipse", "Edit with Eclipse", "Edit Script with Eclipse",
 			new GIcon("icon.plugin.scriptmanager.edit.eclipse"), null, provider::editScriptEclipse);
+
+		createScriptAction("EditVSCode", "Edit with VSCode", "Edit Script with Visual Studio Code",
+			new GIcon("icon.plugin.scriptmanager.edit.vscode"), null, provider::editScriptVSCode);
 
 		keyBindingAction =
 			createScriptAction("Key Binding", "Assign Key Binding", "Assign Key Binding",
