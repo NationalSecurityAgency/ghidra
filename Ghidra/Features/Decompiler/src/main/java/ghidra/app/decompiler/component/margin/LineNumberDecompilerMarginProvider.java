00db743938852d11de469863b3e126e5ef9562f2
Gr-i-niy <grigoriyklopov50@gmail.com>
2024-12-16 20:53:00 +0300

    change: + and - indication

--- Ghidra/Features/Decompiler/src/main/java/ghidra/app/decompiler/component/margin/LineNumberDecompilerMarginProvider.java
+++ Ghidra/Features/Decompiler/src/main/java/ghidra/app/decompiler/component/margin/LineNumberDecompilerMarginProvider.java
@@ -133,7 +133,6 @@ public class LineNumberDecompilerMarginProvider extends JPanel
 		BigInteger startIdx = pixmap.getIndex(visible.y);
 		BigInteger endIdx = pixmap.getIndex(visible.y + visible.height);
 		int ascent = g.getFontMetrics().getMaxAscent();
-		int arrowSize = ascent / 2;
 		Map<Integer, Boolean> linesIndexes = decompilerPanel.getLinesWithOpeningBraces();
 
 
@@ -142,17 +141,11 @@ public class LineNumberDecompilerMarginProvider extends JPanel
 			GraphicsUtils.drawString(this, g, text, leftEdge, pixmap.getPixel(i) + ascent);
 
 			if (linesIndexes.containsKey(i.intValue())) {
-				int y = pixmap.getPixel(i) + ascent;
-				g.setColor(Color.GRAY);
 				if (linesIndexes.get(i.intValue())) {
-					y -= arrowSize / 2;
-					g.drawLine(rightEdge, y, rightEdge - arrowSize / 2, y - arrowSize / 2);
-					g.drawLine(rightEdge, y, rightEdge - arrowSize / 2, y + arrowSize / 2);
+					GraphicsUtils.drawString(this, g, "+", rightEdge, pixmap.getPixel(i) + ascent);
 				} else {
-					g.drawLine(rightEdge, y, rightEdge - arrowSize / 2, y - arrowSize / 2);
-					g.drawLine(rightEdge, y, rightEdge + arrowSize / 2, y - arrowSize / 2);
+					GraphicsUtils.drawString(this, g, "-", rightEdge, pixmap.getPixel(i) + ascent);
 				}
-				g.setColor(Color.BLACK);
 			}
 		}
 	}
