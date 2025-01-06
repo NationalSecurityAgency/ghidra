ea95c146b5f54a72e72d4a8f18e06168d29e0763
Dan <46821332+nsadeveloper789@users.noreply.github.com>
2025-01-06 11:41:55 -0500

    GP-0: Fix some tests (mock.slaspec, register count)

--- Ghidra/Framework/Utility/src/main/java/generic/jar/ResourceFile.java
+++ Ghidra/Framework/Utility/src/main/java/generic/jar/ResourceFile.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,9 +30,10 @@ import utilities.util.FileUtilities;
  */
 
 public class ResourceFile implements Comparable<ResourceFile> {
-	private static final String JAR_FILE_PREFIX = "jar:file:";
-	private Resource resource;
-	private static Map<String, JarResource> jarRootsMap = new HashMap<>();
+	private static final String FILE_PREFIX = "file:";
+	private static final String JAR_FILE_PREFIX = "jar:" + FILE_PREFIX;
+	private final Resource resource;
+	private static final Map<String, JarResource> jarRootsMap = new HashMap<>();
 
 	/**
 	 * Construct a ResourceFile that represents a normal file in the file system.
@@ -93,7 +94,13 @@ public class ResourceFile implements Comparable<ResourceFile> {
 				throw new IllegalArgumentException("Failed to open jar: " + filePath, e);
 			}
 		}
-		resource = new FileResource(new File(absolutePath));
+		else if (absolutePath.startsWith(FILE_PREFIX)) {
+			String filePath = absolutePath.substring(FILE_PREFIX.length());
+			resource = new FileResource(new File(filePath));
+		}
+		else {
+			resource = new FileResource(new File(absolutePath));
+		}
 	}
 
 	/**
