47eeb8b25659c608c6fef555a00f33b3d334313f
ghizard <50744617+ghizard@users.noreply.github.com>
2025-01-22 13:39:05 -0500

    GP-5274 - PDB DefaultCompositeMember - improve alignment, add nopack option

--- Ghidra/Features/PDB/src/main/java/ghidra/app/util/bin/format/pdb/ApplyDataTypes.java
+++ Ghidra/Features/PDB/src/main/java/ghidra/app/util/bin/format/pdb/ApplyDataTypes.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -121,9 +121,9 @@ public class ApplyDataTypes {
 			Composite composite = (Composite) cachedDataType;
 			PdbUtil.clearComponents(composite);
 
-			if (!DefaultCompositeMember.applyDataTypeMembers(composite, compositeDefinition.isClass,
-				compositeDefinition.length, getNormalMembersOnly(compositeDefinition),
-				msg -> Msg.warn(this, msg), monitor)) {
+			if (!DefaultCompositeMember.applyDataTypeMembers(composite, false,
+				compositeDefinition.isClass, compositeDefinition.length,
+				getNormalMembersOnly(compositeDefinition), msg -> Msg.warn(this, msg), monitor)) {
 				PdbUtil.clearComponents(composite);
 			}
 
