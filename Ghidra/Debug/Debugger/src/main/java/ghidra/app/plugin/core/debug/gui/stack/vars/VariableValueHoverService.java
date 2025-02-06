18599e3df6ecacbef9cee5bbc43cf67749ebde88
Dan <46821332+nsadeveloper789@users.noreply.github.com>
2025-02-06 13:18:15 +0000

    GP-3771: Add mask to the unwind analyzer (Fixes unwind with ARM/THUMB)

--- Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/stack/vars/VariableValueHoverService.java
+++ Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/stack/vars/VariableValueHoverService.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -261,8 +261,7 @@ public class VariableValueHoverService extends AbstractConfigurableHover
 			}
 		}
 
-		record MappedLocation(Program stProg, Address stAddr, Address dynAddr) {
-		}
+		record MappedLocation(Program stProg, Address stAddr, Address dynAddr) {}
 
 		protected MappedLocation mapLocation(Program programOrView, Address address) {
 			if (programOrView instanceof TraceProgramView view) {
@@ -400,7 +399,7 @@ public class VariableValueHoverService extends AbstractConfigurableHover
 		public CompletableFuture<VariableValueTable> fillOperand(OperandFieldLocation opLoc,
 				Instruction ins) {
 			RefType refType = ins.getOperandRefType(opLoc.getOperandIndex());
-			if (refType.isFlow()) {
+			if (refType != null && refType.isFlow()) {
 				return null;
 			}
 			Object operand = ins.getDefaultOperandRepresentationList(opLoc.getOperandIndex())
--- Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/stack/SymPcodeArithmetic.java
+++ Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/stack/SymPcodeArithmetic.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,6 +65,8 @@ class SymPcodeArithmetic implements PcodeArithmetic<Sym> {
 				return in1.add(cSpec, in2);
 			case PcodeOp.INT_SUB:
 				return in1.sub(cSpec, in2);
+			case PcodeOp.INT_AND:
+				return in1.and(cSpec, in2);
 			default:
 				return Sym.opaque();
 		}
