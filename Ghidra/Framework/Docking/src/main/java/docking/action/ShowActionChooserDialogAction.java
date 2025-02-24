62488a3f61d6d5b0f8d030b4fd4dad993b86f6f0
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2025-02-24 18:47:46 -0500

    Fixed the Close action to not close the Front End provider

--- Ghidra/Framework/Docking/src/main/java/docking/action/ShowActionChooserDialogAction.java
+++ Ghidra/Framework/Docking/src/main/java/docking/action/ShowActionChooserDialogAction.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,6 +56,9 @@ public class ShowActionChooserDialogAction extends DockingAction {
 
 	private void showActionsDialog(Tool tool, DockingFrame frame, ActionContext context) {
 		ComponentProvider provider = tool.getWindowManager().getActiveComponentProvider();
+		if (provider == null) {
+			return; // this can happen if there is no active provider
+		}
 		ActionChooserDialog actionsDialog = new ActionChooserDialog(tool, provider, context);
 		tool.showDialog(actionsDialog);
 	}
