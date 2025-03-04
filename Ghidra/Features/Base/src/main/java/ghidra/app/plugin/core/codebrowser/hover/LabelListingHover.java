b2785d03642155730d11b79d802b9ac20ac6adf8
ghidragon <106987263+ghidragon@users.noreply.github.com>
2025-03-04 16:17:03 -0500

    GP-4393 add hover to show symbol source type

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/codebrowser/hover/LabelListingHover.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/codebrowser/hover/LabelListingHover.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,8 @@
  */
 package ghidra.app.plugin.core.codebrowser.hover;
 
+import static ghidra.util.HTMLUtilities.*;
+
 import javax.swing.JComponent;
 
 import docking.widgets.fieldpanel.field.Field;
@@ -22,27 +24,25 @@ import docking.widgets.fieldpanel.support.FieldLocation;
 import ghidra.GhidraOptions;
 import ghidra.app.plugin.core.hover.AbstractConfigurableHover;
 import ghidra.framework.plugintool.PluginTool;
-import ghidra.program.model.listing.*;
-import ghidra.program.model.symbol.*;
+import ghidra.program.model.listing.Program;
+import ghidra.program.model.symbol.Symbol;
 import ghidra.program.util.LabelFieldLocation;
 import ghidra.program.util.ProgramLocation;
 
 /**
- * A hover service to show tool tip text for hovering over a label.
- * The tooltip shows the label along with its namespace information.
- * This provides the hover capability for the FunctionNameHoverPlugin and can
- * also be used to directly provide this hover capability to a listing.
+ * A hover service to show the full namespace path of a symbol along with its symbol type and
+ * source type.
  */
-public class FunctionNameListingHover extends AbstractConfigurableHover
+public class LabelListingHover extends AbstractConfigurableHover
 		implements ListingHoverService {
 
-	private static final String NAME = "Function Name Display";
+	private static final String NAME = "Label Display";
 	private static final String DESCRIPTION =
 		"Toggle whether the full symbol name is shown as a tooltip.  This only applies " +
 			"when displaying namespaces.";
 	private static final int PRIORITY = 20;
 
-	public FunctionNameListingHover(PluginTool tool) {
+	public LabelListingHover(PluginTool tool) {
 		super(tool, PRIORITY);
 	}
 
@@ -65,6 +65,14 @@ public class FunctionNameListingHover extends AbstractConfigurableHover
 	public JComponent getHoverComponent(Program program, ProgramLocation programLocation,
 			FieldLocation fieldLocation, Field field) {
 
+		Symbol symbol = getLabelSymbol(programLocation);
+		if (symbol == null) {
+			return null;
+		}
+		return createTooltipComponent(getToolTipText(symbol));
+	}
+
+	private Symbol getLabelSymbol(ProgramLocation programLocation) {
 		if (!enabled || programLocation == null) {
 			return null;
 		}
@@ -73,37 +81,27 @@ public class FunctionNameListingHover extends AbstractConfigurableHover
 			return null;
 		}
 
-		// is the label local to the function
 		Symbol symbol = ((LabelFieldLocation) programLocation).getSymbol();
-		if (isLocalFunctionSymbol(program, symbol)) {
-			return createTooltipComponent(symbol.getName(true));
+		if (symbol == null) {
+			return null;
 		}
 
-		return null;
+		return symbol;
 	}
 
-	private boolean isLocalFunctionSymbol(Program program, Symbol symbol) {
-		if (symbol == null) {
-			return false;
-		}
+	private String getToolTipText(Symbol symbol) {
+		StringBuilder buf = new StringBuilder(HTML);
+		buf.append(friendlyEncodeHTML(symbol.getName(true)));
+		buf.append(BR).append(BR);
+		buf.append("Type: ");
+		buf.append(HTML_SPACE).append(HTML_SPACE).append(HTML_SPACE).append(HTML_SPACE);
+		buf.append(symbol.getSymbolType());
+		buf.append(BR);
+		buf.append("Source: ");
+		buf.append(symbol.getSource().getDisplayString());
+		buf.append(BR);
+		return buf.toString();
 
-		Namespace parentScope = symbol.getParentNamespace();
-		SymbolType symbolType = symbol.getSymbolType();
-
-		if (symbolType != SymbolType.LABEL) {
-			return false;
-		}
-
-		if (parentScope.getID() == Namespace.GLOBAL_NAMESPACE_ID) {
-			return false;
-		}
-
-		FunctionManager functionManager = program.getFunctionManager();
-		Function function = functionManager.getFunctionContaining(symbol.getAddress());
-		if (function == null) {
-			return false;
-		}
-		return function.getName().equals(parentScope.getName());
 	}
 
 }
