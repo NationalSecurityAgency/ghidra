0b0b330bac6786c2d26a75155aaea0ff38f03ff8
ghidragon <106987263+ghidragon@users.noreply.github.com>
2025-03-11 16:13:24 -0400

    GP-5326 created edit structure field action and dialog.

--- Ghidra/Test/IntegrationTest/src/screen/java/help/screenshot/DataPluginScreenShots.java
+++ Ghidra/Test/IntegrationTest/src/screen/java/help/screenshot/DataPluginScreenShots.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,16 @@ public class DataPluginScreenShots extends GhidraScreenShotGenerator {
 		JRadioButton button = (JRadioButton) getInstanceField("exactMatchButton", dialog);
 		setSelected(button, true);
 		captureDialog(500, 400);
+	}
+
+	@Test
+	public void testEditFieldDialog() {
+		positionListingTop(0x400080);
+		positionCursor(0x400080, "+");
+		leftClickCursor();
+		positionListingTop(0x4000a4);
+		performAction("Edit Field", "DataPlugin", false);
+		captureDialog();
 	}
 
 	@Test
