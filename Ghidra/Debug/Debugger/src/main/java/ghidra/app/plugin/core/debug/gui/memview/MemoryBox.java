7ed42228a02e4bb3655d5adc1ddf35033ced2266
d-millar <33498836+d-millar@users.noreply.github.com>
2025-03-11 16:40:01 -0400

    GP-0: memview mods

--- Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/memview/MemoryBox.java
+++ Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/memview/MemoryBox.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,16 +42,20 @@ public class MemoryBox {
 	protected int pixTstart;
 	protected int pixTend;
 	protected int boundT;
-	protected final Color color;
+	protected Color color;
 
 	protected boolean current;
 
-	public MemoryBox(String id, MemviewBoxType type, AddressRange range, long tick) {
+	public MemoryBox(String id, MemviewBoxType type, AddressRange range, long tick, Color color) {
 		this.id = id;
 		this.type = type;
 		this.range = range;
 		this.start = tick;
-		this.color = type.getColor();
+		this.color = color;
+	}
+
+	public MemoryBox(String id, MemviewBoxType type, AddressRange range, long tick) {
+		this(id, type, range, tick, type.getColor());
 	}
 
 	public MemoryBox(String id, MemviewBoxType type, AddressRange range, Lifespan trange) {
@@ -89,6 +93,10 @@ public class MemoryBox {
 
 	public Color getColor() {
 		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
 	}
 
 	public int getAddressPixelStart() {
