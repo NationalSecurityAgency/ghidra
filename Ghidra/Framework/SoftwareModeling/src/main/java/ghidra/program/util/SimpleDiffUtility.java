905b4d78bdd7c597b52c942e5086d67dc36e8d9e
ghidra1 <ghidra1@users.noreply.github.com>
2025-03-13 18:48:42 -0400

    GP-5480 Corrected Merge tool rendering of external locations within multi-listing.

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/util/SimpleDiffUtility.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/util/SimpleDiffUtility.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -250,7 +250,7 @@ public class SimpleDiffUtility {
 		else if (addr.isExternalAddress()) {
 			Symbol s = program.getSymbolTable().getPrimarySymbol(addr);
 			if (s != null && s.isExternal()) {
-				s = getSymbol(s, otherProgram);
+				s = getMatchingExternalSymbol(program, s, otherProgram, null);
 				if (s != null) {
 					return s.getAddress();
 				}
