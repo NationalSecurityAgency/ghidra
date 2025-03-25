907e5c563f2d74cee824da625190dec888147b99
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2025-03-25 09:28:03 -0400

    GP-5521: pyghidra.open_program() now accepts a program_name parameter, which can be used to override the program name derived from the binary_path parameter

--- Ghidra/Features/PyGhidra/src/main/py/src/pyghidra/__init__.py
+++ Ghidra/Features/PyGhidra/src/main/py/src/pyghidra/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##
-__version__ = "2.0.2"
+__version__ = "2.1.0"
 
 # stub for documentation and typing
 # this is mostly to hide the function parameter
