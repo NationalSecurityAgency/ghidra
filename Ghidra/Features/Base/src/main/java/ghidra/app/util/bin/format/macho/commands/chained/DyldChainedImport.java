c1a424dcde347cde922b61d87b3416d1f02c67b2
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2025-04-21 08:40:02 -0400

    GP-0: Fixing Mach-O DYLD_CHAINED_IMPORT_ADDEND64 weak_import value (Closes #8023)

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/macho/commands/chained/DyldChainedImport.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/macho/commands/chained/DyldChainedImport.java
@@ -63,7 +63,7 @@ public class DyldChainedImport implements StructConverter {
 			case DYLD_CHAINED_IMPORT_ADDEND64: {
 				long ival = reader.readNextLong();
 				lib_ordinal = (int) (ival & 0xffff);
-				weak_import = ((ival >> 8) & 1) == 1;
+				weak_import = ((ival >> 16) & 1) == 1;
 				name_offset = ((ival >> 32) & 0xffffffffL);
 				addend = reader.readNextLong();
 				break;
