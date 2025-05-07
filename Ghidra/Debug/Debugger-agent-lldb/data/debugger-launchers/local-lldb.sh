bd2088dfedd7589a3307413833c288a067f72acc
NapCat <BBH@awsl.rip>
2025-05-07 20:44:59 +1000

    Update local-lldb.sh, fix #8115

--- Ghidra/Debug/Debugger-agent-lldb/data/debugger-launchers/local-lldb.sh
+++ Ghidra/Debug/Debugger-agent-lldb/data/debugger-launchers/local-lldb.sh
@@ -55,14 +55,14 @@ if [ -z "$target_args" ]
 then
   argspart=
 else
-  argspart=-o "settings set target.run-args $target_args"
+  argspart=("-o" "settings set target.run-args $target_args")
 fi
 
 if [ -z "$TARGET_TTY" ]
 then
   ttypart=
 else
-  ttypart=-o "settings set target.output-path $TTY_TARGET" -o "settings set target.input-path $TTY_TARGET"
+  ttypart=("-o" "settings set target.output-path $TTY_TARGET" "-o" "settings set target.input-path $TTY_TARGET")
 fi
 
 if [ -z "$target_image" ]
@@ -70,7 +70,7 @@ then
   "$OPT_LLDB_PATH" \
     -o "version" \
     -o "script import ghidralldb" \
-    $ttypart \
+    "${ttypart[@]}" \
     -o "ghidra trace connect \"$GHIDRA_TRACE_RMI_ADDR\"" \
     -o "ghidra trace start" \
     -o "ghidra trace sync-enable"
@@ -79,8 +79,8 @@ else
     -o "version" \
     -o "script import ghidralldb" \
     -o "target create \"$target_image\"" \
-    $argspart \
-    $ttypart \
+    "${argspart[@]}" \
+    "${ttypart[@]}" \
     -o "ghidra trace connect \"$GHIDRA_TRACE_RMI_ADDR\"" \
     -o "ghidra trace start" \
     -o "ghidra trace sync-enable" \
