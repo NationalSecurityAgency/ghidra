265757cf7dfe5717b96ab1a7fc1293e8125dea5d
ghidra1 <ghidra1@users.noreply.github.com>
2025-05-08 17:18:19 -0400

    GP-0 Corrected NPE for eBPF ELF import. (Closes #8034)

--- Ghidra/Processors/eBPF/src/main/java/ghidra/app/util/bin/format/elf/relocation/eBPF_ElfRelocationHandler.java
+++ Ghidra/Processors/eBPF/src/main/java/ghidra/app/util/bin/format/elf/relocation/eBPF_ElfRelocationHandler.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,9 +47,10 @@ public class eBPF_ElfRelocationHandler
 		Program program = elfRelocationContext.getProgram();
 		Memory memory = program.getMemory();
 
-		String section_name =
-			elfRelocationContext.relocationTable.getSectionToBeRelocated().getNameAsString();
-		if (section_name.toString().contains("debug")) {
+		ElfSectionHeader sectionToBeRelocated =
+		elfRelocationContext.relocationTable.getSectionToBeRelocated();
+		if (sectionToBeRelocated != null &&
+				sectionToBeRelocated.getNameAsString().startsWith(".debug")) {
 			return RelocationResult.SKIPPED;
 		}
 
