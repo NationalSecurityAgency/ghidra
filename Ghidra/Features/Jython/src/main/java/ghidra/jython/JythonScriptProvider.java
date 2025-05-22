5ec12f5dafaa88c8494987b8deefa8652ccfa39f
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2025-05-22 09:29:49 -0400

    GP-5415: Python scripts that do not declare a @runtime metadata comment now default to PyGhidra instead of Jython. (Closes #7856)

--- Ghidra/Features/Jython/src/main/java/ghidra/jython/JythonScriptProvider.java
+++ Ghidra/Features/Jython/src/main/java/ghidra/jython/JythonScriptProvider.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,12 +19,10 @@ import java.io.PrintWriter;
 
 import generic.jar.ResourceFile;
 import ghidra.app.script.*;
-import ghidra.util.classfinder.ExtensionPointProperties;
 
 /**
  * A {@link GhidraScriptProvider} used to run Jython scripts
  */
-@ExtensionPointProperties(priority = 1000) // Enforce high priority so Jython is the default Python provider
 public class JythonScriptProvider extends AbstractPythonScriptProvider {
 
 	@Override
