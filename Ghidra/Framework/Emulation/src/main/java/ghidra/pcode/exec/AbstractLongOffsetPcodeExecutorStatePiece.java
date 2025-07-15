4bc44fc943c8ec3ba35b629d16fe459bea806961
ghintern <ghintern@localhost>
2025-07-15 14:30:29 +0000

    GP-5829: respect emulator quantize flag for addressing

--- Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/exec/AbstractLongOffsetPcodeExecutorStatePiece.java
+++ Ghidra/Framework/Emulation/src/main/java/ghidra/pcode/exec/AbstractLongOffsetPcodeExecutorStatePiece.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -296,7 +296,9 @@ public abstract class AbstractLongOffsetPcodeExecutorStatePiece<A, T, S>
 			return;
 		}
 		S s = getForSpace(space, true);
-		offset = quantizeOffset(space, offset);
+		if (quantize) {
+			offset = quantizeOffset(space, offset);
+		}
 		setInSpace(s, offset, size, val);
 	}
 
@@ -320,7 +322,9 @@ public abstract class AbstractLongOffsetPcodeExecutorStatePiece<A, T, S>
 		if (s == null) {
 			return getFromNullSpace(size, reason);
 		}
-		offset = quantizeOffset(space, offset);
+		if (quantize) {
+			offset = quantizeOffset(space, offset);
+		}
 		return getFromSpace(s, offset, size, reason);
 	}
 
