ffb7e653ccdb2abae6055da95ab8db370f84039c
ghidra1 <ghidra1@users.noreply.github.com>
2025-10-15 09:45:54 -0400

    GP-6039 Address parsing docs.  Allow ProgramUtilities.parseAddress to handle external address and removed support for parsing block-name style addresses

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/address/DefaultAddressFactory.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/address/DefaultAddressFactory.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -147,9 +147,6 @@ public class DefaultAddressFactory implements AddressFactory {
 		}
 	}
 
-	/**
-	 * @see ghidra.program.model.address.AddressFactory#getAddress(java.lang.String)
-	 */
 	@Override
 	public Address getAddress(String addrString) {
 		try {
@@ -218,7 +215,6 @@ public class DefaultAddressFactory implements AddressFactory {
 		}
 
 		Address[] addrs = new Address[loadedMemoryList.size()];
-
 		return loadedMemoryList.toArray(addrs);
 	}
 
