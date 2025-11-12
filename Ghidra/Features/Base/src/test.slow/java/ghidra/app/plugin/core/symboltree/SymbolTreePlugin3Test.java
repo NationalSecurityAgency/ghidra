e8f2db974093c13b5536a523bcacbbc9cfad7a68
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2025-11-12 14:00:47 -0500

    GP-6101 - Symbol Tree - Added a toolbar toggle action to control outgoing navigations

--- Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/symboltree/SymbolTreePlugin3Test.java
+++ Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/symboltree/SymbolTreePlugin3Test.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,6 +36,7 @@ import docking.widgets.tree.support.GTreeNodeTransferable;
 import ghidra.app.plugin.core.codebrowser.CodeBrowserPlugin;
 import ghidra.app.plugin.core.marker.MarkerManagerPlugin;
 import ghidra.app.plugin.core.programtree.ProgramTreePlugin;
+import ghidra.app.plugin.core.symboltree.actions.NavigateOnIncomingAction;
 import ghidra.app.plugin.core.symboltree.nodes.*;
 import ghidra.app.util.viewer.field.LabelFieldFactory;
 import ghidra.framework.options.SaveState;
@@ -186,10 +187,10 @@ public class SymbolTreePlugin3Test extends AbstractGhidraHeadedIntegrationTest {
 
 	@Test
 	public void testDragDropLabelOnClass() throws Exception {
-		final ToggleDockingAction goToToggleAction =
-			(ToggleDockingAction) getAction(plugin, "Navigation");
+		final ToggleDockingAction navigateIncomingAction =
+			(ToggleDockingAction) getAction(plugin, NavigateOnIncomingAction.NAME);
 
-		runSwing(() -> goToToggleAction.setSelected(true));
+		runSwing(() -> navigateIncomingAction.setSelected(true));
 		GTreeNode cNode = rootNode.getChild(4);
 		GTreeNode nsNode = util.createObject(cNode, "MyClass", createClassAction);
 		assertNotNull(nsNode);
@@ -333,10 +334,10 @@ public class SymbolTreePlugin3Test extends AbstractGhidraHeadedIntegrationTest {
 
 	@Test
 	public void testSaveState() throws Exception {
-		final ToggleDockingAction goToToggleAction =
-			(ToggleDockingAction) getAction(plugin, "Navigation");
-		assertNotNull(goToToggleAction);
-		runSwing(() -> goToToggleAction.setSelected(true));
+		final ToggleDockingAction navigateIncomingAction =
+			(ToggleDockingAction) getAction(plugin, NavigateOnIncomingAction.NAME);
+		assertNotNull(navigateIncomingAction);
+		runSwing(() -> navigateIncomingAction.setSelected(true));
 
 		SaveState saveState = new SaveState("Test");
 		plugin.writeConfigState(saveState);
