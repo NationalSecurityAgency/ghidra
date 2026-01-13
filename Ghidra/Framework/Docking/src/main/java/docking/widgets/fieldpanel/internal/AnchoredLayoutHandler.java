2c99fbe7f61e922eced0078ecb2535fb1954cf1d
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2026-01-13 09:38:34 -0500

    Revert "GP-6299: Bail from getPreviousLayout if there are no layouts."
    This reverts commit 02c51234f46ea6f6ba48871d3db973e3ec615cb2.

--- Ghidra/Framework/Docking/src/main/java/docking/widgets/fieldpanel/internal/AnchoredLayoutHandler.java
+++ Ghidra/Framework/Docking/src/main/java/docking/widgets/fieldpanel/internal/AnchoredLayoutHandler.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -195,9 +195,6 @@ public class AnchoredLayoutHandler {
 			Layout layout = model.getLayout(index);
 			if (layout != null) {
 				return new AnchoredLayout(layout, index, yPos - layout.getHeight());
-			}
-			if (layouts.isEmpty()) {
-				return null;
 			}
 		}
 		return null;
