70342d0cd81f90da0b6eeadc13c5645c3ec411dc
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2026-01-20 14:37:03 -0500

    GP-6323 - Updated program and trace tabs to have the concept of selected and active/inactive

--- Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/listing/DebuggerListingProvider.java
+++ Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/listing/DebuggerListingProvider.java
@@ -33,8 +33,7 @@ import javax.swing.event.ChangeListener;
 import org.apache.commons.lang3.StringUtils;
 import org.jdom.Element;
 
-import docking.ActionContext;
-import docking.WindowPosition;
+import docking.*;
 import docking.action.DockingAction;
 import docking.action.ToggleDockingAction;
 import docking.action.builder.ToggleActionBuilder;
@@ -172,15 +171,15 @@ public class DebuggerListingProvider extends CodeViewerProvider {
 		}
 
 		@Override
-		protected void specChanged(LocationTrackingSpec spec) {
+		protected void specChanged(LocationTrackingSpec lts) {
 			if (isMainListing()) {
-				plugin.firePluginEvent(new TrackingChangedPluginEvent(getName(), spec));
+				plugin.firePluginEvent(new TrackingChangedPluginEvent(getName(), lts));
 			}
 			updateTitle();
 			trackingLabel.setText("");
 			trackingLabel.setToolTipText("");
 			trackingLabel.setForeground(Colors.FOREGROUND);
-			trackingSpecChangeListeners.invoke().locationTrackingSpecChanged(spec);
+			trackingSpecChangeListeners.invoke().locationTrackingSpecChanged(lts);
 		}
 
 		@Override
@@ -347,6 +346,8 @@ public class DebuggerListingProvider extends CodeViewerProvider {
 
 	private long countAddressesInIndex;
 
+	private TabContextListener contextListener;
+
 	public DebuggerListingProvider(DebuggerListingPlugin plugin, FormatManager formatManager,
 			boolean isConnected) {
 		super(plugin, formatManager, isConnected);
@@ -378,6 +379,9 @@ public class DebuggerListingProvider extends CodeViewerProvider {
 
 		if (isConnected) {
 			traceTabs = new DebuggerTraceTabPanel(plugin);
+			contextListener = new TabContextListener();
+			DockingWindowManager dwm = tool.getWindowManager();
+			dwm.addContextListener(contextListener);
 		}
 		else {
 			traceTabs = null;
@@ -1043,4 +1047,44 @@ public class DebuggerListingProvider extends CodeViewerProvider {
 		}
 		return new DebuggerByteSource(tool, current.getView(), current.getTarget(), readsMemTrait);
 	}
+
+	private class TabContextListener implements DockingContextListener {
+
+		@Override
+		public void contextChanged(ActionContext localContext) {
+
+			DockingWindowManager dwm = tool.getWindowManager();
+			DebuggerProgramLocationActionContext defaultContext =
+				(DebuggerProgramLocationActionContext) dwm
+						.getDefaultActionContext(DebuggerProgramLocationActionContext.class);
+			Trace myTrace = null;
+			if (defaultContext != null) {
+				TraceProgramView tpv = defaultContext.getProgram();
+				myTrace = tpv.getTrace();
+			}
+
+			if (!(localContext instanceof DebuggerProgramLocationActionContext dlac)) {
+
+				// Future: We would like to make the debugger be the default context in this case, 
+				// but we need a way to have the static and dynamic views to decide who is in charge.
+				// For now, assume it should always be the static non-debugger listing view, which
+				// means making the trace tabs inactive.
+				traceTabs.setActive(false);
+				return;
+			}
+
+			TraceProgramView localTraceProgramView = dlac.getProgram();
+			Trace localTrace = localTraceProgramView.getTrace();
+			if (myTrace != localTrace || !dlac.isActiveProgram()) {
+				// A different trace is in the local context; deactivate out tabs.
+				traceTabs.setActive(false);
+				return;
+			}
+
+			// Signal that the trace from our default context is the active trace.
+			traceTabs.setActive(true);
+		}
+
+	}
+
 }
--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/progmgr/MultiTabPlugin.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/progmgr/MultiTabPlugin.java
@@ -19,14 +19,15 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
-import docking.ActionContext;
-import docking.DockingUtils;
+import docking.*;
 import docking.action.*;
 import docking.action.builder.ActionBuilder;
 import docking.tool.ToolConstants;
 import docking.widgets.tab.GTabPanel;
 import generic.theme.GIcon;
 import ghidra.app.CorePluginPackage;
+import ghidra.app.context.ListingActionContext;
+import ghidra.app.context.ProgramActionContext;
 import ghidra.app.events.*;
 import ghidra.app.plugin.PluginCategoryNames;
 import ghidra.app.services.CodeViewerService;
@@ -87,6 +88,7 @@ public class MultiTabPlugin extends Plugin
 	private DockingAction goToPreviousProgramAction;
 
 	private Timer selectHighlightedProgramTimer;
+	private TabContextListener contextListener = new TabContextListener();
 
 	public MultiTabPlugin(PluginTool tool) {
 		super(tool);
@@ -267,6 +269,9 @@ public class MultiTabPlugin extends Plugin
 		progService = tool.getService(ProgramManager.class);
 		cvService = tool.getService(CodeViewerService.class);
 		cvService.setNorthComponent(tabPanel);
+
+		DockingWindowManager dwm = tool.getWindowManager();
+		dwm.addContextListener(contextListener);
 	}
 
 	private void initOptions() {
@@ -294,11 +299,7 @@ public class MultiTabPlugin extends Plugin
 		return EMPTY8_ICON;
 	}
 
-	boolean removeProgram(Program program) {
-		return progService.closeProgram(program, false);
-	}
-
-	void programSelected(Program program) {
+	private void programSelected(Program program) {
 		if (program != progService.getCurrentProgram()) {
 			progService.setCurrentProgram(program);
 			cvService.requestFocus();
@@ -403,6 +404,52 @@ public class MultiTabPlugin extends Plugin
 	@Override
 	public void undoRedoOccurred(DomainObjectAdapterDB domainObj) {
 		tabPanel.refreshTab((Program) domainObj);
+	}
+
+//=================================================================================================
+// Inner Classes
+//=================================================================================================
+
+	private class TabContextListener implements DockingContextListener {
+
+		@Override
+		public void contextChanged(ActionContext localContext) {
+
+			/*
+			 	 Goal: We would like to have the program tabs paint as gray when the default context
+			 	 	   is not being driven by the active program.   This should happen whenever the
+			 	 	   focus is in a component that has a program that can be used by tool actions.
+			 	 	   
+			 	 	   The tool uses the active program as a fallback/default for actions when the 
+			 	 	   local context will not work.  When a local context has a program different 
+			 	 	   than the active program, then we want to signal to users visually that the 
+			 	 	   focused component is the one providing the program for the current context.
+			 */
+
+			DockingWindowManager dwm = tool.getWindowManager();
+			Program myProgram = null;
+			ProgramActionContext defaultContext =
+				(ListingActionContext) dwm.getDefaultActionContext(ProgramActionContext.class);
+			if (defaultContext != null) {
+				myProgram = defaultContext.getProgram();
+			}
+
+			if (!(localContext instanceof ProgramActionContext pac)) {
+				tabPanel.setActive(true);
+				return;
+			}
+
+			Program localProgram = pac.getProgram();
+			if (myProgram != localProgram || !pac.isActiveProgram()) {
+				// A different program is in the local context; deactivate out tabs.
+				tabPanel.setActive(false);
+				return;
+			}
+
+			// Signal that the program from our default context is the active program.
+			tabPanel.setActive(true);
+		}
+
 	}
 
 }
