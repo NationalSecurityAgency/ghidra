fc63ec39436638fe5ba1f7def9555d5529900c99
d-millar <33498836+d-millar@users.noreply.github.com>
2026-02-09 21:04:59 +0000

    GP-6417: move buttons

--- Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/control/TargetStepExtAction.java
+++ Ghidra/Debug/Debugger/src/main/java/ghidra/app/plugin/core/debug/gui/control/TargetStepExtAction.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ import ghidra.util.HelpLocation;
 interface TargetStepExtAction extends ControlAction {
 	Icon ICON = DebuggerResources.ICON_STEP_LAST;
 	String HELP_ANCHOR = "target_step_ext";
-	int SUB_GROUP = 9;
+	int SUB_GROUP = 7;
 	KeyStroke KEY_BINDING = KeyStroke.getKeyStroke(KeyEvent.VK_F8, KeyEvent.CTRL_DOWN_MASK);
 
 	static ActionBuilder builder(String name, DebuggerControlPlugin owner) {
