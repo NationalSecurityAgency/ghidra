f7f890c71f412886033003c3362b1ac1b2377740
emteere <47253321+emteere@users.noreply.github.com>
2026-03-11 18:34:37 +0000

    GP-6553 Adding tests for i64 and other constant variants

--- Ghidra/Features/Base/src/test/resources/ghidra/app/util/cparser/CParserTest.h
+++ Ghidra/Features/Base/src/test/resources/ghidra/app/util/cparser/CParserTest.h
@@ -1189,8 +1189,37 @@ void multilineStrings(void)
     char b[] = "This is a "
                "multiline string.";
 }
- 
 
+/**
+ ** Constants
+ **/
+ /* Decimal */
+ int                 dec_int    = 28;
+ unsigned            dec_uint   = 4000000024u;
+ long                dec_long   = 2000000022l;
+ unsigned long       dec_ulong  = 4000000000ul;
+ long long           dec_llong  = 9000000000LL;
+ unsigned long long  dec_ullong = 900000000001ull;
+ __int64             dec_i64    = 9000000000002I64;
+ unsigned __int64    dec_ui64   = 90000000000004ui64;
+ /* Octal */
+ int                 oct_int    = 024;
+ unsigned            oct_uint   = 04000000024u;
+ long                oct_long   = 02000000022l;
+ unsigned long       oct_ulong  = 04000000000UL;
+ long long           oct_llong  = 044000000000000ll;
+ unsigned long long  oct_ullong = 044400000000000001Ull;
+ __int64             oct_i64    = 04444000000000000002i64;
+ unsigned __int64    oct_ui64   = 04444000000000000004uI64;
+ /* Hexadecimal  */
+ int                 hex_int    = 0x2a;
+ unsigned            hex_uint   = 0XA0000024u;
+ long                hex_long   = 0x20000022l;
+ unsigned long       hex_ulong  = 0XA0000021uL;
+ long long           hex_llong  = 0x8a000000000000ll;
+ unsigned long long  hex_ullong = 0x8A40000000000010uLL;
+ __int64             hex_i64    = 0x4a44000000000020I64;
+ unsigned __int64    hex_ui64   = 0x8a44000000000040Ui64;
 
 /**
  ** #line in structure/function body
