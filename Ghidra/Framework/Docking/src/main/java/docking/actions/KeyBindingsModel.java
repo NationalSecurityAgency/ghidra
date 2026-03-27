03bd71d0122ccd4d0867e24acac226e6a2823042
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2026-03-27 19:02:55 -0400

    GP-6628 - Key Bindings - Updated the options UI to show unregistered key bindings

--- Ghidra/Framework/Docking/src/main/java/docking/actions/KeyBindingsModel.java
+++ Ghidra/Framework/Docking/src/main/java/docking/actions/KeyBindingsModel.java
@@ -17,6 +17,7 @@ package docking.actions;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import javax.swing.KeyStroke;
 
@@ -30,19 +31,19 @@ import gui.event.MouseBinding;
 import util.CollectionUtils;
 
 /**
- * An object that maps actions to key strokes and mouse bindings.
+ * An object that maps actions to key strokes and mouse bindings.  This class loads key bindings 
+ * from tool options.  Clients can use this model to manage the values of each binding until the 
+ * user is done editing the bindings.  When the client is finished, the bindings changes can be:
+ * applied, ignored or restored to default settings.
  * <p>
  * This class knows how to load all system actions and how to load any key and mouse bindings for
  * those actions from the tool's options.   Clients can make changes to the state of this class that
  * can then be applied to the system by calling {@link #applyChanges()}.
  */
-public class KeyBindings {
+public class KeyBindingsModel {
 
 	private Tool tool;
 	private ToolOptions keyBindingOptions;
-
-	// allows clients to populate a table of all actions
-	private List<DockingActionIf> uniqueActions = new ArrayList<>();
 
 	// allows clients to know if a given key stroke or mouse binding is in use
 	private Map<KeyStroke, List<String>> actionNamesByKeyStroke = new HashMap<>();
@@ -50,11 +51,11 @@ public class KeyBindings {
 
 	// tracks all changes to an action's key stroke and mouse bindings, which allows us to apply
 	// and restore options values
-	private Map<String, ActionKeyBindingState> actionInfoByFullName = new HashMap<>();
+	private Map<String, KeyBindingState> actionInfoByFullName = new HashMap<>();
 
 	private String longestActionName = "";
 
-	public KeyBindings(Tool tool) {
+	public KeyBindingsModel(Tool tool) {
 		this.tool = tool;
 
 		keyBindingOptions = tool.getOptions(DockingToolConstants.KEY_BINDINGS);
@@ -62,15 +63,19 @@ public class KeyBindings {
 		init();
 	}
 
-	public List<DockingActionIf> getUniqueActions() {
-		return Collections.unmodifiableList(uniqueActions);
+	public List<ActionBindingsDescriptor> getActionBindings() {
+
+		return actionInfoByFullName.values()
+				.stream()
+				.map(state -> (ActionBindingsDescriptor) state)
+				.collect(Collectors.toList());
 	}
 
 	/* used for testing */
 	public Map<String, KeyStroke> getKeyStrokesByFullActionName() {
 		Map<String, KeyStroke> result = new HashMap<>();
-		Set<Entry<String, ActionKeyBindingState>> entries = actionInfoByFullName.entrySet();
-		for (Entry<String, ActionKeyBindingState> entry : entries) {
+		Set<Entry<String, KeyBindingState>> entries = actionInfoByFullName.entrySet();
+		for (Entry<String, KeyBindingState> entry : entries) {
 			String key = entry.getKey();
 			KeyStroke value = entry.getValue().getCurrentKeyStroke();
 			result.put(key, value);
@@ -83,12 +88,12 @@ public class KeyBindings {
 	}
 
 	public KeyStroke getKeyStroke(String fullName) {
-		ActionKeyBindingState info = actionInfoByFullName.get(fullName);
+		KeyBindingState info = actionInfoByFullName.get(fullName);
 		return info.getCurrentKeyStroke();
 	}
 
 	public MouseBinding getMouseBinding(String fullName) {
-		ActionKeyBindingState info = actionInfoByFullName.get(fullName);
+		KeyBindingState info = actionInfoByFullName.get(fullName);
 		return info.getCurrentMouseBinding();
 	}
 
@@ -114,11 +119,9 @@ public class KeyBindings {
 			sb.append("  ");
 
 			String name = names.get(i);
-			ActionKeyBindingState info = actionInfoByFullName.get(name);
-			DockingActionIf action = info.getRepresentativeAction();
-			String shortName = action.getName();
-			sb.append(shortName);
-			sb.append(" (").append(action.getOwnerDescription()).append(')');
+			KeyBindingState state = actionInfoByFullName.get(name);
+			String fullName = state.getFullName();
+			sb.append(fullName);
 			if (i < names.size() - 1) {
 				sb.append("\n");
 			}
@@ -155,7 +158,7 @@ public class KeyBindings {
 			actionNameByMouseBinding.put(newBinding, fullName);
 		}
 
-		ActionKeyBindingState info = actionInfoByFullName.get(fullName);
+		KeyBindingState info = actionInfoByFullName.get(fullName);
 		info.setCurrentMouseBinding(newBinding);
 		return true;
 	}
@@ -174,14 +177,14 @@ public class KeyBindings {
 		}
 		addActionKeyStroke(fullName, newKs);
 
-		ActionKeyBindingState info = actionInfoByFullName.get(fullName);
+		KeyBindingState info = actionInfoByFullName.get(fullName);
 		info.setCurrentKeyStroke(newKs);
 		return true;
 	}
 
 	public boolean removeKeyStroke(String fullName) {
 
-		ActionKeyBindingState info = actionInfoByFullName.get(fullName);
+		KeyBindingState info = actionInfoByFullName.get(fullName);
 		if (info == null) {
 			return false; // not sure if this can happen
 		}
@@ -201,7 +204,7 @@ public class KeyBindings {
 	 * system started.
 	 */
 	public void restoreOptions() {
-		for (ActionKeyBindingState info : actionInfoByFullName.values()) {
+		for (KeyBindingState info : actionInfoByFullName.values()) {
 			info.restore(keyBindingOptions);
 		}
 	}
@@ -210,8 +213,8 @@ public class KeyBindings {
 	 * Cancels any pending changes that have not yet been applied.
 	 */
 	public void cancelChanges() {
-		for (ActionKeyBindingState info : actionInfoByFullName.values()) {
-			info.cancelChanges();
+		for (KeyBindingState info : actionInfoByFullName.values()) {
+			info.clearChanges();
 		}
 	}
 
@@ -219,7 +222,7 @@ public class KeyBindings {
 	 * Applies any pending changes.
 	 */
 	public void applyChanges() {
-		for (ActionKeyBindingState info : actionInfoByFullName.values()) {
+		for (KeyBindingState info : actionInfoByFullName.values()) {
 			info.apply(keyBindingOptions);
 		}
 	}
@@ -242,6 +245,7 @@ public class KeyBindings {
 
 		actionInfoByFullName = new HashMap<>();
 
+		Set<String> registeredNames = new HashSet<>();
 		Map<String, List<DockingActionIf>> actionsByFullName =
 			KeyBindingUtils.getAllActionsByFullName(tool);
 		Set<Entry<String, List<DockingActionIf>>> entries = actionsByFullName.entrySet();
@@ -252,6 +256,8 @@ public class KeyBindings {
 			String fullName = entry.getKey();
 			ActionTrigger trigger = keyBindingOptions.getActionTrigger(fullName, null);
 
+			registeredNames.add(fullName);
+
 			KeyStroke ks = null;
 			MouseBinding mb = null;
 
@@ -260,17 +266,44 @@ public class KeyBindings {
 				mb = trigger.getMouseBinding();
 			}
 
-			ActionKeyBindingState info = new ActionKeyBindingState(actions, ks, mb);
-			actionInfoByFullName.put(fullName, info);
-
-			uniqueActions.add(info.getRepresentativeAction());
+			ActionKeyBindingState state = new ActionKeyBindingState(actions, ks, mb);
+			actionInfoByFullName.put(fullName, state);
 
 			addActionKeyStroke(fullName, ks);
 
-			String shortName = info.getShortName();
+			String shortName = state.getName();
 			if (shortName.length() > longestActionName.length()) {
 				longestActionName = shortName;
 			}
+		}
+
+		// ask options for unregistered key binding options
+		List<String> allNamesList = keyBindingOptions.getOptionNames();
+		Set<String> unregisteredNames = new HashSet<>(allNamesList);
+		unregisteredNames.removeAll(registeredNames);
+
+		for (String fullName : unregisteredNames) {
+			KeyStroke ks = null;
+			MouseBinding mb = null;
+
+			ActionTrigger trigger = keyBindingOptions.getActionTrigger(fullName, null);
+			if (trigger != null) {
+				ks = trigger.getKeyStroke();
+				mb = trigger.getMouseBinding();
+			}
+
+			UnregisteredActionKeyBindingState state =
+				new UnregisteredActionKeyBindingState(fullName, ks, mb);
+			actionInfoByFullName.put(fullName, state);
+
+			addActionKeyStroke(fullName, ks);
+
+			int description = fullName.indexOf("(");
+			String shortName = fullName.substring(0, description).trim();
+			if (shortName.length() > longestActionName.length()) {
+				longestActionName = shortName;
+			}
+
 		}
 	}
 
@@ -289,62 +322,74 @@ public class KeyBindings {
 		}
 	}
 
-	/**
-	 * A class to store current and original values for key strokes and mouse bindings.  This is 
-	 * used to apply changes and restore default values.
-	 */
-	private class ActionKeyBindingState {
+	private abstract class KeyBindingState implements ActionBindingsDescriptor {
 
-		private List<DockingActionIf> actions = new ArrayList<>();
-		private KeyStroke originalKeyStroke;
-		private KeyStroke currentKeyStroke;
-		private MouseBinding originalMouseBinding;
-		private MouseBinding currentMouseBinding;
+		KeyStroke originalKeyStroke;
+		KeyStroke currentKeyStroke;
+		MouseBinding originalMouseBinding;
+		MouseBinding currentMouseBinding;
 
-		ActionKeyBindingState(List<DockingActionIf> actions, KeyStroke ks, MouseBinding mb) {
-			this.actions.addAll(actions);
+		KeyBindingState(KeyStroke ks, MouseBinding mb) {
 			this.originalKeyStroke = ks;
 			this.currentKeyStroke = ks;
 			this.originalMouseBinding = mb;
 			this.currentMouseBinding = mb;
 		}
 
-		public DockingActionIf getRepresentativeAction() {
-			// pick one action, they are all conceptually the same
-			return actions.get(0);
+		abstract KeyBindingData getCurrentKeyBindingData();
+
+		// restores the key/mouse binding options to their default values
+		abstract void restore(ToolOptions options);
+
+		KeyBindingData getOriginalKeyBindingData() {
+
+			if (originalKeyStroke == null && originalMouseBinding == null) {
+				return null; // the key binding data does not exist or has been cleared
+			}
+
+			ActionTrigger trigger = new ActionTrigger(originalKeyStroke, originalMouseBinding);
+			return new KeyBindingData(trigger);
 		}
 
-		String getShortName() {
-			// pick one action, they are all conceptually the same
-			return actions.get(0).getName();
+		@Override
+		public String getBindingText() {
+			String text = "";
+			String fullName = getFullName();
+			KeyStroke ks = getKeyStroke(fullName);
+			if (ks != null) {
+				text += KeyBindingUtils.parseKeyStroke(ks);
+			}
+
+			MouseBinding mb = getMouseBinding(fullName);
+			if (mb != null) {
+				text += " (" + mb.getDisplayText() + ")";
+			}
+
+			return text.trim();
 		}
 
-		String getFullName() {
-			return getRepresentativeAction().getFullName();
-		}
-
-		public MouseBinding getCurrentMouseBinding() {
+		MouseBinding getCurrentMouseBinding() {
 			return currentMouseBinding;
 		}
 
-		public void setCurrentMouseBinding(MouseBinding newMouseBinding) {
+		void setCurrentMouseBinding(MouseBinding newMouseBinding) {
 			this.currentMouseBinding = newMouseBinding;
 		}
 
-		public KeyStroke getCurrentKeyStroke() {
+		KeyStroke getCurrentKeyStroke() {
 			return currentKeyStroke;
 		}
 
-		public void setCurrentKeyStroke(KeyStroke newKeyStroke) {
+		void setCurrentKeyStroke(KeyStroke newKeyStroke) {
 			this.currentKeyStroke = newKeyStroke;
 		}
 
-		public void cancelChanges() {
+		void clearChanges() {
 			currentKeyStroke = originalKeyStroke;
 			currentMouseBinding = originalMouseBinding;
 		}
 
-		public void apply(ToolOptions keyStrokeOptions) {
+		void apply(ToolOptions keyStrokeOptions) {
 			if (!hasChanged()) {
 				return;
 			}
@@ -353,19 +398,17 @@ public class KeyBindings {
 			apply(keyStrokeOptions, kbd);
 		}
 
-		private void apply(ToolOptions keyStrokeOptions, KeyBindingData keyBinding) {
-
+		void apply(ToolOptions keyStrokeOptions, KeyBindingData keyBinding) {
+			String fullName = getFullName();
 			if (keyBinding == null) {
-				// no bindings; bindings have been cleared
-				for (DockingActionIf action : actions) {
-					action.setUnvalidatedKeyBindingData(null);
-				}
+				keyStrokeOptions.setActionTrigger(fullName, null);
+				currentKeyStroke = null;
+				currentMouseBinding = null;
 				return;
 			}
 
 			// 1) update the options with the new value
 			ActionTrigger newTrigger = keyBinding.getActionTrigger();
-			String fullName = getFullName();
 			keyStrokeOptions.setActionTrigger(fullName, newTrigger);
 
 			// 2) update our state so the UI shows the new value
@@ -373,12 +416,7 @@ public class KeyBindings {
 			currentMouseBinding = newTrigger.getMouseBinding();
 		}
 
-		private boolean hasChanged() {
-			return !Objects.equals(originalKeyStroke, currentKeyStroke) ||
-				!Objects.equals(originalMouseBinding, currentMouseBinding);
-		}
-
-		private boolean matches(KeyBindingData kbData) {
+		boolean matches(KeyBindingData kbData) {
 
 			if (CollectionUtils.isAllNull(kbData, currentKeyStroke, currentMouseBinding)) {
 				return true;
@@ -397,7 +435,137 @@ public class KeyBindings {
 			return Objects.equals(otherMb, currentMouseBinding);
 		}
 
-		private KeyBindingData getCurrentKeyBindingData() {
+		boolean hasChanged() {
+			return !Objects.equals(originalKeyStroke, currentKeyStroke) ||
+				!Objects.equals(originalMouseBinding, currentMouseBinding);
+		}
+	}
+
+	/**
+	 * Represents info for an action that is known by the options, but has not been registered.
+	 */
+	private class UnregisteredActionKeyBindingState extends KeyBindingState {
+
+		private String fullName;
+		private String actionName;
+		private String ownerName;
+
+		UnregisteredActionKeyBindingState(String fullName, KeyStroke ks, MouseBinding mb) {
+			super(ks, mb);
+			this.fullName = fullName;
+
+			int descriptionIndex = fullName.indexOf("(");
+			this.actionName = fullName.substring(0, descriptionIndex).trim();
+
+			int lastParen = fullName.lastIndexOf(")");
+			this.ownerName = fullName.substring(descriptionIndex + 1, lastParen).trim();
+		}
+
+		@Override
+		public String getName() {
+			return actionName;
+		}
+
+		@Override
+		public String getFullName() {
+			return fullName;
+		}
+
+		@Override
+		public String getOwnerDescription() {
+			return ownerName;
+		}
+
+		@Override
+		public String getDescription() {
+			return ""; // no action; no description
+		}
+
+		@Override
+		public DockingActionIf getRepresentativeAction() {
+			return null; // no action
+		}
+
+		@Override
+		public boolean isRegistered() {
+			return false;
+		}
+
+		@Override
+		KeyBindingData getCurrentKeyBindingData() {
+			if (currentKeyStroke == null && currentMouseBinding == null) {
+				return null; // no bindings or the values have been cleared
+			}
+
+			ActionTrigger trigger = new ActionTrigger(currentKeyStroke, currentMouseBinding);
+			return new KeyBindingData(trigger);
+		}
+
+		@Override
+		void restore(ToolOptions options) {
+
+			if (!hasChanged()) {
+				return;
+			}
+
+			KeyBindingData defaultBinding = getOriginalKeyBindingData();
+
+			clearChanges();
+
+			if (!matches(defaultBinding)) {
+				apply(options, defaultBinding);
+			}
+		}
+
+	}
+
+	/**
+	 * A class to store current and original values for key strokes and mouse bindings.  This is 
+	 * used to apply changes and restore default values.
+	 */
+	private class ActionKeyBindingState extends KeyBindingState {
+
+		private List<DockingActionIf> actions = new ArrayList<>();
+
+		ActionKeyBindingState(List<DockingActionIf> actions, KeyStroke ks, MouseBinding mb) {
+			super(ks, mb);
+			this.actions.addAll(actions);
+		}
+
+		@Override
+		public String getName() {
+			// pick one action, they are all conceptually the same
+			return getRepresentativeAction().getName();
+		}
+
+		@Override
+		public String getFullName() {
+			return getRepresentativeAction().getFullName();
+		}
+
+		@Override
+		public String getOwnerDescription() {
+			return getRepresentativeAction().getOwnerDescription();
+		}
+
+		@Override
+		public String getDescription() {
+			return getRepresentativeAction().getDescription();
+		}
+
+		@Override
+		public DockingActionIf getRepresentativeAction() {
+			// pick one action, they are all conceptually the same
+			return actions.get(0);
+		}
+
+		@Override
+		public boolean isRegistered() {
+			return true;
+		}
+
+		@Override
+		KeyBindingData getCurrentKeyBindingData() {
 
 			if (currentKeyStroke == null && currentMouseBinding == null) {
 				return null; // the key binding data does not exist or has been cleared
@@ -410,11 +578,12 @@ public class KeyBindings {
 		}
 
 		// restores the options to their default values
-		public void restore(ToolOptions options) {
+		@Override
+		void restore(ToolOptions options) {
 			DockingActionIf action = getRepresentativeAction();
 			KeyBindingData defaultBinding = action.getDefaultKeyBindingData();
 
-			cancelChanges();
+			clearChanges();
 
 			if (!matches(defaultBinding)) {
 				apply(options, defaultBinding);
