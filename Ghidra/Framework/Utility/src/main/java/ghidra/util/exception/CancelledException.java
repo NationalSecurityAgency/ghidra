37f27db75dfd8e28e12422c2299d2a3ea2f9063e
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2026-05-15 16:53:03 -0400

    GP-6837 - Function Graph - Added an option to limit the number of nodes

--- Ghidra/Framework/Utility/src/main/java/ghidra/util/exception/CancelledException.java
+++ Ghidra/Framework/Utility/src/main/java/ghidra/util/exception/CancelledException.java
@@ -1,13 +1,12 @@
 /* ###
  * IP: GHIDRA
- * REVIEWED: YES
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,22 +15,30 @@
  */
 package ghidra.util.exception;
 
-
 /**
  * <code>CancelledException</code> indicates that the user cancelled
  * the current operation.
  */
 public class CancelledException extends UsrException {
 
+	public static final String DEFAULT_MESSAGE = "Operation cancelled";
+
 	/**
 	 * Default constructor.  Message indicates 'Operation cancelled'.
 	 */
 	public CancelledException() {
-		super("Operation cancelled");
+		super(DEFAULT_MESSAGE);
 	}
 
 	public CancelledException(String msg) {
 		super(msg);
 	}
-	
+
+	/**
+	 * {@return true if the message of this exception is {@value #DEFAULT_MESSAGE}}
+	 */
+	public boolean isDefaultMessage() {
+		return DEFAULT_MESSAGE.equals(getMessage());
+	}
+
 }
