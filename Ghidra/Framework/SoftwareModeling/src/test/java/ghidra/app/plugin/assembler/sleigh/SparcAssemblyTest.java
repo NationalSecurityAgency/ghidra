fb4bfe5aebeef7aa91d352611bee95742cabe4c6
ghidorahrex <ghidorahrex@users.noreply.github.com>
2026-06-25 11:30:07 -0400

    GP-0: Corrected SparcAssemblyTest

--- Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/app/plugin/assembler/sleigh/SparcAssemblyTest.java
+++ Ghidra/Framework/SoftwareModeling/src/test/java/ghidra/app/plugin/assembler/sleigh/SparcAssemblyTest.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ public class SparcAssemblyTest extends AbstractAssemblyTest {
 
 	@Test
 	public void testAssemble_rd_Y_g2() {
-		assertOneCompatRestExact("rd %Y,g2", "85:40:00:00");
+		assertOneCompatRestExact("rd %Y,%g2", "85:40:00:00");
 	}
 
 	// Too many reserved/undefined bits: 524288 instructions to test
@@ -38,6 +38,6 @@ public class SparcAssemblyTest extends AbstractAssemblyTest {
 
 	@Test
 	public void testAssemble_wr_g3_g0() {
-		assertOneCompatRestExact("wr g0,g3,%Y", "81:80:00:03");
+		assertOneCompatRestExact("wr %g0,%g3,%Y", "81:80:00:03");
 	}
 }
