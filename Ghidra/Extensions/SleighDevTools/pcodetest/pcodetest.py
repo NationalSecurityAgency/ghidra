NationalSecurityAgency/ghidra

commit 71cd33572c873b1eebd322032506c259632d0d52

GP-6007: Updated NDS32 analyzer and elf relocation handler and test fixups

Ghidra/Extensions/SleighDevTools/pcodetest/pcodetest.py
@@ -88,7 +88,13 @@ class PCodeTestBuild(BuildUtil):
     def main(self):
 
         # make sure compiler exists and runnable
-
+        self.config.compile_exe = self.config.exec_dir + self.config.exec_prefix + self.config.compile_exe
+        self.config.build_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.build_exe)
+        self.config.strip_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.strip_exe)
+        self.config.objdump_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.objdump_exe)
+        self.config.readelf_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.readelf_exe)
+        self.config.nm_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.nm_exe)
+        
         if not self.is_executable_file(self.which('compile_exe')):
             self.log_err(self.config.format('build the Toolchain before compilation'))
             return
@@ -427,6 +433,7 @@ class PCodeBuildGCC(PCodeTestBuild):
         self.set_library_path(self.config.ld_library_path)
 
         # Construct the compile/link command line and execute it
+            
         cmp = self.which('compile_exe')
         cmd = [cmp] + input_files + self.cflags(output_file)  + [opt_cflag, '-B', self.dirname(cmp), '-o', output_file]
         out, err = self.run(cmd)

Ghidra/Extensions/SleighDevTools/pcodetest/c_src/misc_BODY.c
@@ -16,6 +16,14 @@
 #include "pcode_test.h"
 #include "big_struct.h"
 
+#ifdef HAS_LIBC
+#include <string.h>
+#else
+void *memset(void *b, int c, size_t len);
+void *memcpy(void *dst, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+#endif
+
 static i4 int_expectedValue;
 static i4 int_actualValue;
 

Ghidra/Extensions/SleighDevTools/pcodetest/defaults.py
@@ -47,12 +47,14 @@ pcodeTestDefaults.has_vector = 0
 pcodeTestDefaults.small_build = 0
 pcodeTestDefaults.ld_library_path = ''
 pcodeTestDefaults.toolchain_type = 'gcc'
-pcodeTestDefaults.compile_exe = 'bin/gcc'
-pcodeTestDefaults.objdump_exe = 'bin/objdump'
+pcodeTestDefaults.compile_exe = 'gcc'
+pcodeTestDefaults.objdump_exe = 'objdump'
 pcodeTestDefaults.objdump_option = ''
-pcodeTestDefaults.readelf_exe = 'bin/readelf'
-pcodeTestDefaults.nm_exe = 'bin/nm'
-pcodeTestDefaults.strip_exe = 'bin/strip'
+pcodeTestDefaults.readelf_exe = 'readelf'
+pcodeTestDefaults.exec_dir = 'bin/'
+pcodeTestDefaults.exec_prefix = ''
+pcodeTestDefaults.nm_exe = 'nm'
+pcodeTestDefaults.strip_exe = 'strip'
 pcodeTestDefaults.variants = {'O0': '-O0', 'O3': '-O3'}
 pcodeTestDefaults.proc_test = ''
 pcodeTestDefaults.force = False

Ghidra/Extensions/SleighDevTools/pcodetest/pcode_defs.py
@@ -436,7 +436,7 @@ PCodeTest({
     'name': 'NDS32BE',
     'build_all': 1,
     'toolchain': 'NDS32/nds32be-elf',
-    'ccflags': '',
+    'ccflags': '-mbig-endian',
     'cclibs': '-lgcc',
     'language_id': 'NDS32:BE:32:default',
 })
@@ -444,9 +444,20 @@ PCodeTest({
 PCodeTest({
     'name': 'NDS32LE',
     'build_all': 1,
-    'toolchain': 'NDS32/nds32le-elf',
+    'toolchain': 'NDS32/nds32le-elf',#'NDS32/nds32le-linux-glibc-v5d',
+    'ccflags': '-mlittle-endian -EL',
+    'cclibs': '-lgcc',
+    'language_id': 'NDS32:LE:32:default',
+})
+
+PCodeTest({
+    'name': 'NDS32AS',
+    'build_all': 1,
+    'toolchain': 'NDS32/nds32le-elf-mculib-v3s',
+    'exec_prefix': 'nds32le-elf-',
     'ccflags': '',
     'cclibs': '-lgcc',
+    'gcc_version': '12.2.0',
     'language_id': 'NDS32:LE:32:default',
 })
 

Ghidra/Processors/NDS32/build.gradle
@@ -1,3 +1,18 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 apply from: "$rootProject.projectDir/gradle/distributableGhidraModule.gradle"
 apply from: "$rootProject.projectDir/gradle/javaProject.gradle"
 apply from: "$rootProject.projectDir/gradle/jacocoProject.gradle"

Ghidra/Processors/NDS32/certification.manifest
@@ -1,0 +1,13 @@
+##VERSION: 2.0
+Module.manifest||GHIDRA||||END|
+data/languages/lsmw.sinc||GHIDRA||||END|
+data/languages/nds32.cspec||GHIDRA||||END|
+data/languages/nds32.dwarf||GHIDRA||||END|
+data/languages/nds32.ldefs||GHIDRA||||END|
+data/languages/nds32.opinion||GHIDRA||||END|
+data/languages/nds32.pspec||GHIDRA||||END|
+data/languages/nds32.sinc||GHIDRA||||END|
+data/languages/nds32be.slaspec||GHIDRA||||END|
+data/languages/nds32le.slaspec||GHIDRA||||END|
+data/patterns/nds32_patterns.xml||GHIDRA||||END|
+data/patterns/patternconstraints.xml||GHIDRA||||END|

Ghidra/Processors/NDS32/data/buildLanguage.xml
@@ -1,50 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<!--
-  + Compile sleigh languages within this module.
-  + Sleigh compiler options are read from the sleighArgs.txt file.
-  + Eclipse: right-click on this file and choose menu item "Run As->Ant Build"
-  -->
-                                     
-<project name="privateBuildDeveloper" default="sleighCompile">
-	
-	<property name="sleigh.compile.class" value="ghidra.pcodeCPort.slgh_compile.SleighCompile"/>
-
-	<!--Import optional ant properties.  GhidraDev Eclipse plugin produces this so this file can find the Ghidra installation-->
-	<import file="../.antProperties.xml" optional="false" />
-	
-	<target name="sleighCompile">
-	    
-		<!-- If language module is detached from installation, get Ghidra installation directory path from imported properties -->
-		<property name="framework.path" value="${ghidra.install.dir}/Ghidra/Framework"/>
-		
-		<path id="sleigh.class.path">
-			<fileset dir="${framework.path}/SoftwareModeling/lib">
-				<include name="*.jar"/>
-			</fileset>
-			<fileset dir="${framework.path}/Generic/lib">
-				<include name="*.jar"/>
-			</fileset>
-			<fileset dir="${framework.path}/Utility/lib">
-				<include name="*.jar"/>
-			</fileset>
-		</path>
-		
-		<available classname="${sleigh.compile.class}" classpathref="sleigh.class.path" property="sleigh.compile.exists"/>
-			
-		<fail unless="sleigh.compile.exists" />
-		
-		<java classname="${sleigh.compile.class}"
-			classpathref="sleigh.class.path"
-			fork="true"
-			failonerror="true">
-			<jvmarg value="-Xmx2048M"/>
-			<arg value="-i"/>
-			<arg value="sleighArgs.txt"/>
-			<arg value="-a"/>
-			<arg value="./languages"/>
-		</java>
-		
- 	</target>
-
-</project>

commit 276c607260eaa873ba4bc38a3c503442df74d01d

Do not normalize build_exe in pcodetests definitions
In pcodetests definition, build_exe is an integer set to 1 when an
executable may be built. Contrary to other *_exe fields, it is not the
name of a command and therefore should not be normalized with exec_dir.

Fixes: 71cd33572c87 ("GP-6007: Updated NDS32 analyzer and elf relocation
handler and test fixups")

Ghidra/Extensions/SleighDevTools/pcodetest/pcodetest.py
@@ -91,7 +91,6 @@ class PCodeTestBuild(BuildUtil):
 
         # make sure compiler exists and runnable
         self.config.compile_exe = self.config.exec_dir + self.config.exec_prefix + self.config.compile_exe
-        self.config.build_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.build_exe)
         self.config.strip_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.strip_exe)
         self.config.objdump_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.objdump_exe)
         self.config.readelf_exe = self.config.exec_dir + str(self.config.exec_prefix) + str(self.config.readelf_exe)