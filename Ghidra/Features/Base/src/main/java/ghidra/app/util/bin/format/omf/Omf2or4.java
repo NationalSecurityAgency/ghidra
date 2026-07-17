db839765545caf825cb7ca73f4664312e7287256
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2026-07-17 08:38:44 -0400

    GP-7071: Fixing potential infinite loop in the PefLoader, and some other PEF/OMF parsing fixes

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/Omf2or4.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/Omf2or4.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,36 +23,11 @@ import ghidra.util.exception.DuplicateNameException;
 
 /**
  * An OMF value that is either 2 or 4 bytes
+ *
+ * @param length 2 or 4
+ * @param value The 2 or 4 byte value
  */
-public class Omf2or4 implements StructConverter {
-
-	private int length;
-	private long value;
-
-	/**
-	 * Creates a new {@link Omf2or4}
-	 * 
-	 * @param length 2 or 4
-	 * @param value The 2 or 4 byte value
-	 */
-	public Omf2or4(int length, long value) {
-		this.length = length;
-		this.value = value;
-	}
-
-	/**
-	 * {@return the length of the value (2 or 4)}
-	 */
-	public int length() {
-		return length;
-	}
-
-	/**
-	 * {@return the value}
-	 */
-	public long value() {
-		return value;
-	}
+public record Omf2or4(int length, long value) implements StructConverter {
 
 	@Override
 	public DataType toDataType() throws DuplicateNameException, IOException {
