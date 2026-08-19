2eadd6ae13aa789ca1c1aab2f26d2d6efa6817f7
dragonmacher <48328597+dragonmacher@users.noreply.github.com>
2026-08-19 17:57:02 -0400

    GP-7144 - Updated the Front End to use a natural sort for project files

--- Ghidra/Framework/Generic/src/test/java/ghidra/util/datastruct/AlphaNumericComparatorTest.java
+++ Ghidra/Framework/Generic/src/test/java/ghidra/util/datastruct/AlphaNumericComparatorTest.java
@@ -26,7 +26,7 @@ import generic.test.AbstractGenericTest;
 public class AlphaNumericComparatorTest extends AbstractGenericTest {
 
 	@Test
-	public void testIt() {
+	public void testCaseSensitive() {
 
 		List<String> unsorted = Arrays.asList(
 
@@ -78,6 +78,63 @@ public class AlphaNumericComparatorTest extends AbstractGenericTest {
 		Collections.shuffle(unsorted);
 		List<String> copy = new ArrayList<>(unsorted);
 		Collections.sort(copy, new AlphaNumericComparator());
+
+		assertEquals(sorted, copy);
+	}
+
+	@Test
+	public void testCaseInsensitive() {
+
+		List<String> unsorted = Arrays.asList(
+
+			"  file10",       // Leading spaces
+			"file02  ",       // Trailing spaces
+			"file  2",        // Internal spaces
+			"file1",          // Normal  
+			"  file002  ",    // Leading and trailing spaces
+			"file 01",        // Internal space before digit
+			"file20",
+
+			"  apple",        // Leading spaces
+			"Apple  ",        // Trailing spaces
+			"ba  nana",       // Internal spaces
+			"a",
+			"alpha  ",        // Trailing spaces
+			"  alphabet",     // Leading spaces
+			"app",
+			"ap  ple",         // Internal spaces
+
+			"0.1.0",
+			"0.1.9",
+			"1.0",
+			"0.2.1",
+			"2.1.0");
+
+		List<String> sorted = Arrays.asList(
+			"0.1.0",
+			"0.1.9",
+			"0.2.1",
+			"1.0",
+			"2.1.0",
+			"a",
+			"alpha  ",
+			"  alphabet",
+			"app",
+			"  apple",
+			"Apple  ",
+			"ap  ple",
+			"ba  nana",
+			"file1",
+			"file 01",
+			"file  2",
+			"file02  ",
+			"  file002  ",
+			"  file10",
+			"file20");
+
+		Collections.shuffle(unsorted);
+		List<String> copy = new ArrayList<>(unsorted);
+		Collections.sort(copy, new AlphaNumericComparator(false));
 
 		assertEquals(sorted, copy);
 	}
